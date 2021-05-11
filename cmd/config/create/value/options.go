33a4e73a5095d75d6470674383436a04d9b7ba1b
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-05-11 18:40:20 +0200

    Improve validation

--- cmd/config/create/value/options.go
+++ cmd/config/create/value/options.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/kyma-incubator/reconciler/internal/cli"
 )
 
@@ -14,4 +16,14 @@ type Options struct {
 
 func NewOptions(o *cli.Options) *Options {
 	return &Options{o, "", 0, "", ""}
+}
+
+func (o *Options) Validate() error {
+	if o.Bucket == "" {
+		return fmt.Errorf("Bucket has to be specified")
+	}
+	if o.Key == "" && o.KeyVersion <= 0 {
+		return fmt.Errorf("Either the key or the key-version has to be provided")
+	}
+	return nil
 }
