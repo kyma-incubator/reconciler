3408b8d21e06ca3932c77ad6b3936b118557b111
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-05-14 12:39:13 +0200

    Fix bug in time parsing pattern

--- pkg/config/entity.go
+++ pkg/config/entity.go
@@ -16,7 +16,7 @@ const (
 //convertTimestampToTime is converting the value of timestamp db-column to a Time instance
 func convertTimestampToTime(value interface{}) (interface{}, error) {
 	if reflect.TypeOf(value).Kind() == reflect.String {
-		layout := "2006-02-01 15:04:05"
+		layout := "2006-01-02 15:04:05" //see https://golang.org/src/time/format.go
 		return time.Parse(layout, value.(string))
 	}
 	if time, ok := value.(time.Time); ok {
