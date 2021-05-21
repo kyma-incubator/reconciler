3722ece38e2d0d421ac7e1de4315989b35507a6a
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-05-21 20:45:54 +0200

    Move  connfactory creator-fct to db package

--- pkg/config/repository_test.go
+++ pkg/config/repository_test.go
@@ -1,38 +1,21 @@
 package config
 
 import (
-	"fmt"
 	"io/ioutil"
 	"path"
 
 	"github.com/kyma-incubator/reconciler/pkg/db"
-	"github.com/spf13/viper"
 )
 
 func newTestConnectionFactory() (db.ConnectionFactory, error) {
-	viper.SetConfigFile(path.Join("test", "reconciler-test.yaml"))
-	if err := viper.ReadInConfig(); err != nil {
-		return nil, err
+	connFac, err := db.NewConnectionFactory(path.Join("test", "reconciler-test.yaml"), "configManagement")
+	if err != nil {
+		return connFac, err
 	}
-	dbToUse := viper.GetString("configManagement.db.driver")
-	switch dbToUse {
-	case "postgres":
-		return &db.PostgresConnectionFactory{
-			Host:     viper.GetString("configManagement.db.postgres.host"),
-			Port:     viper.GetInt("configManagement.db.postgres.port"),
-			Database: viper.GetString("configManagement.db.postgres.database"),
-			User:     viper.GetString("configManagement.db.postgres.user"),
-			Password: viper.GetString("configManagement.db.postgres.password"),
-			SslMode:  viper.GetBool("configManagement.db.postgres.sslMode"),
-			Debug:    true,
-		}, nil
-	case "sqlite":
-		conFac := &db.SqliteConnectionFactory{
-			File:  viper.GetString("configManagement.db.sqlite.file"),
-			Debug: true,
-		}
+
+	if _, ok := connFac.(*db.SqliteConnectionFactory); ok {
 		//get connection
-		conn, err := conFac.NewConnection()
+		conn, err := connFac.NewConnection()
 		if err != nil {
 			panic(err)
 		}
@@ -48,8 +31,8 @@ func newTestConnectionFactory() (db.ConnectionFactory, error) {
 		if err != nil {
 			panic(err)
 		}
-		return conFac, nil
-	default:
-		panic(fmt.Sprintf("DB type '%s' not supported", dbToUse))
 	}
+
+	return connFac, nil
+
 }
