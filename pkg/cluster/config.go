806b1e053b46e790b2108d48bea2bef7b629a5ce
Suleyman Akbas <suleyman.akbas@sap.com>
2021-06-17 17:24:08 +0200

    Improve .gitignore and makefile (#23)
    * Improve .gitignore and makefile
    
    * improve
    
    * improve
    
    * Fix

--- pkg/cluster/config.go
+++ pkg/cluster/config.go
@@ -1,20 +1,9 @@
 package cluster
 
-import (
-	"github.com/kyma-incubator/reconciler/pkg/kv"
-	"github.com/kyma-incubator/reconciler/pkg/model"
-)
-
-var defaultBucketPrecedence = []string{model.DefaultBucket, "profile", "customer", "cluster", "feature"}
-
 type Configuration struct {
-	cluster      string
-	bucketMerger *bucketMerger
 }
 
 type Configurer struct {
-	kvRepository *kv.Repository
-	inventory    *Inventory
 }
 
 func (c *Configurer) Get(cluster string) *Configuration {
