23bcec8f422e66090911f082f1801f4cd1aaacf1
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-06-27 13:03:47 +0200

    improve provider

--- pkg/chart/provider.go
+++ pkg/chart/provider.go
@@ -1,19 +1,49 @@
 package chart
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/kyma-incubator/hydroform/parallel-install/pkg/components"
 	"github.com/kyma-incubator/hydroform/parallel-install/pkg/config"
 	"github.com/kyma-incubator/hydroform/parallel-install/pkg/deployment"
 	"github.com/kyma-incubator/hydroform/parallel-install/pkg/overrides"
-	"github.com/kyma-incubator/reconciler/pkg/db"
 	log "github.com/kyma-incubator/reconciler/pkg/logger"
+	"github.com/kyma-incubator/reconciler/pkg/model"
 	"github.com/kyma-incubator/reconciler/pkg/workspace"
+	"github.com/pkg/errors"
+)
+
+const (
+	defaultWorkersCount  = 4
+	defaultCancelTimeout = 2 * time.Minute
+	defaultQuitTimeout   = 3 * time.Minute
 )
 
 type Provider struct {
 	Debug bool
+}
+
+type Options struct {
+	WorkersCount  int
+	CancelTimeout time.Duration
+	QuitTimeout   time.Duration
+}
+
+func (o *Options) validate() error {
+	if o.WorkersCount <= 0 {
+		return fmt.Errorf("WorkersCount cannot be < 0")
+	}
+	if o.WorkersCount == 0 {
+		o.WorkersCount = defaultWorkersCount
+	}
+	if o.CancelTimeout.Microseconds() == int64(0) {
+		o.CancelTimeout = defaultCancelTimeout
+	}
+	if o.QuitTimeout.Microseconds() == int64(0) {
+		o.QuitTimeout = defaultQuitTimeout
+	}
+	return nil
 }
 
 func (p *Provider) loggerAdapter() (*HydroformLoggerAdapter, error) {
@@ -24,7 +54,11 @@ func (p *Provider) loggerAdapter() (*HydroformLoggerAdapter, error) {
 	return NewHydroformLoggerAdapter(logger), nil
 }
 
-func (p *Provider) Manifests(cluster *db.DatabaseEntity, ws *workspace.Workspace) ([]*components.Manifest, error) {
+func (p *Provider) Manifests(cluster *model.ClusterPropertyEntity /* << fix me */, ws *workspace.Workspace, opts *Options) ([]*components.Manifest, error) {
+	if err := opts.validate(); err != nil {
+		return nil, errors.Wrap(err, "Invalid options provided")
+	}
+
 	logger, err := p.loggerAdapter()
 	if err != nil {
 		return nil, err
@@ -36,22 +70,24 @@ func (p *Provider) Manifests(cluster *db.DatabaseEntity, ws *workspace.Workspace
 	}
 
 	cfg := &config.Config{
-		WorkersCount:                  4,
-		CancelTimeout:                 20 * time.Minute,
-		QuitTimeout:                   25 * time.Minute,
-		HelmTimeoutSeconds:            60 * 8,
+		WorkersCount:                  opts.WorkersCount,
+		CancelTimeout:                 opts.CancelTimeout,
+		QuitTimeout:                   opts.QuitTimeout,
+		HelmTimeoutSeconds:            60,
 		BackoffInitialIntervalSeconds: 3,
-		BackoffMaxElapsedTimeSeconds:  60 * 5,
+		BackoffMaxElapsedTimeSeconds:  45,
 		Log:                           logger,
 		HelmMaxRevisionHistory:        10,
 		ComponentList:                 compList,
 		ResourcePath:                  ws.ResourceDir,
 		InstallationResourcePath:      ws.InstallationResourceDir,
-		KubeconfigSource: config.KubeconfigSource{
-			Path:    cluster.KubeConfig, //FIXME
-			Content: "",
-		},
-		Version: cluster.Version, //FIXME
+
+		//FIXME:
+		// KubeconfigSource: config.KubeconfigSource{
+		// 	Path:    cluster.KubeConfig, //FIXME
+		// 	Content: "",
+		// },
+		// Version: cluster.Version, //FIXME
 	}
 
 	builder := &overrides.Builder{} //TODO: merge config from cluster model
