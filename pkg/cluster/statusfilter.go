679c1c996e06474807c48a5c20c0e2c54f3641ce
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-07-05 21:54:49 +0200

    Use sql-filter entites instead of parameters

---
+++ pkg/cluster/statusfilter.go
@@ -0,0 +1,59 @@
+package cluster
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/kyma-incubator/reconciler/pkg/db"
+	"github.com/kyma-incubator/reconciler/pkg/model"
+)
+
+type statusSQLFilter interface {
+	Filter(dbType db.Type, colHdr *db.ColumnHandler) (string, error)
+}
+
+type statusFilter struct {
+	allowedStatuses []model.Status
+}
+
+func (sf *statusFilter) Filter(dbType db.Type, statusColHdr *db.ColumnHandler) (string, error) {
+	statusColName, err := statusColHdr.ColumnName("Status")
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s IN ('%s')", statusColName, strings.Join(sf.stausesToStrings(), "','")), nil
+}
+
+func (sf *statusFilter) stausesToStrings() []string {
+	result := []string{}
+	for _, status := range sf.allowedStatuses {
+		result = append(result, string(status))
+	}
+	return result
+}
+
+type reconcileIntervalFilter struct {
+	reconcileInterval time.Duration
+}
+
+func (rif *reconcileIntervalFilter) Filter(dbType db.Type, statusColHdr *db.ColumnHandler) (string, error) {
+	statusColName, err := statusColHdr.ColumnName("Status")
+	if err != nil {
+		return "", err
+	}
+	createdColName, err := statusColHdr.ColumnName("Created")
+	if err != nil {
+		return "", err
+	}
+	switch dbType {
+	case db.Postgres:
+		return fmt.Sprintf(`%s = '%s' AND %s >= NOW() - INTERVAL '%.0f SECOND'`,
+			statusColName, model.Ready, createdColName, rif.reconcileInterval.Seconds()), nil
+	case db.SQLite:
+		return fmt.Sprintf(`%s = '%s' AND %s >= DATETIME('now', '-%.0f SECONDS')`,
+			statusColName, model.Ready, createdColName, rif.reconcileInterval.Seconds()), nil
+	default:
+		return "", fmt.Errorf("Database type '%s' is not supported by this filter", dbType)
+	}
+}
