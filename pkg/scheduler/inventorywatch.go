a6668576e871f20c178cfbc8a608857acfddd3e0
Krystian Cieslik <3641496+crabtree@users.noreply.github.com>
2021-07-15 13:29:06 +0200

    wip

--- pkg/scheduler/inventorywatch.go
+++ pkg/scheduler/inventorywatch.go
@@ -12,7 +12,7 @@ import (
 type InventoryQueue chan<- cluster.State
 
 type InventoryWatcher interface {
-	Run(ctx context.Context, informer InventoryQueue)
+	Run(ctx context.Context, informer InventoryQueue) error
 }
 
 func NewInventoryWatch(inventory cluster.Inventory, logger *zap.Logger) (InventoryWatcher, error) {
@@ -24,30 +24,37 @@ type DefaultInventoryWatcher struct {
 	logger    *zap.Logger
 }
 
-func (w *DefaultInventoryWatcher) Run(ctx context.Context, informer InventoryQueue) {
+func (w *DefaultInventoryWatcher) Run(ctx context.Context, queue InventoryQueue) error {
 loop:
 	for {
 		select {
 		case <-ctx.Done():
 			break loop
 		default:
-			clusters, err := w.inventory.ClustersToReconcile(1 * time.Minute)
-			if err != nil {
-				w.logger.Error(fmt.Sprintf("Error while fetching clusters to reconcile from inventory: %s", err))
-			}
-
-			if len(clusters) == 0 {
-				time.Sleep(500 * time.Millisecond)
-				continue
-			}
-
-			for _, cluster := range clusters {
-				if cluster == nil {
-					w.logger.Debug("Nil cluster state when processing clusters to reconcile list")
-					continue
-				}
-				informer <- *cluster
-			}
+			w.processClustersToReconcile(queue)
 		}
+	}
+
+	return nil
+}
+
+func (w *DefaultInventoryWatcher) processClustersToReconcile(queue InventoryQueue) {
+	clusters, err := w.inventory.ClustersToReconcile(1 * time.Minute)
+	if err != nil {
+		w.logger.Error(fmt.Sprintf("Error while fetching clusters to reconcile from inventory: %s", err))
+		return
+	}
+
+	if len(clusters) == 0 {
+		time.Sleep(500 * time.Millisecond)
+		return
+	}
+
+	for _, cluster := range clusters {
+		if cluster == nil {
+			w.logger.Debug("Nil cluster state when processing the list of clusters to reconcile")
+			continue
+		}
+		queue <- *cluster
 	}
 }
--- pkg/scheduler/inventorywatch_test.go
+++ pkg/scheduler/inventorywatch_test.go
@@ -1,15 +1,59 @@
 package scheduler
 
 import (
+	"context"
+	"sync"
 	"testing"
 
 	"github.com/kyma-incubator/reconciler/pkg/cluster"
+	"github.com/kyma-incubator/reconciler/pkg/model"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestInventoryWatch(t *testing.T) {
 	inventory := &cluster.MockInventory{}
+	dummyState := mockState()
+	inventory.ClustersToReconcileResult = []*cluster.State{dummyState}
+	queue := make(chan cluster.State, 1)
+	ctx, cancelFn := context.WithCancel(context.TODO())
+	defer cancelFn()
+
 	inventoryWatch, err := NewInventoryWatch(inventory, nil)
 	require.NoError(t, err)
-	_ = inventoryWatch
+
+	go inventoryWatch.Run(ctx, queue)
+	clusterState := <-queue
+
+	require.NotEmpty(t, clusterState)
+	require.Equal(t, "foo", clusterState.Status.Cluster)
+}
+
+func TestInventoryWatch_ShouldStopOnCtxClose(t *testing.T) {
+	inventory := &cluster.MockInventory{}
+	queue := make(chan cluster.State, 1)
+	ctx, cancelFn := context.WithCancel(context.TODO())
+
+	inventoryWatch, err := NewInventoryWatch(inventory, nil)
+	require.NoError(t, err)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		err := inventoryWatch.Run(ctx, queue)
+		assert.NoError(t, err)
+		wg.Done()
+	}()
+	cancelFn()
+	wg.Wait()
+}
+
+func mockState() *cluster.State {
+	return &cluster.State{
+		Cluster:       &model.ClusterEntity{},
+		Configuration: &model.ClusterConfigurationEntity{},
+		Status: &model.ClusterStatusEntity{
+			Cluster: "foo",
+		},
+	}
 }
