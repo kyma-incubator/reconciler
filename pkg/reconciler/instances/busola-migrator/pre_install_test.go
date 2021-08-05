f67873063ab28e875a66fffe95aa2441e0208512
Damian Badura <damian.badura@sap.com>
2021-08-05 10:39:26 +0200

    add all unit tests

--- pkg/reconciler/instances/busola-migrator/pre_install_test.go
+++ pkg/reconciler/instances/busola-migrator/pre_install_test.go
@@ -10,7 +10,6 @@ import (
 	"github.com/stretchr/testify/require"
 	"io"
 	"io/ioutil"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	restFake "k8s.io/client-go/rest/fake"
 	"net/http"
 	"path"
@@ -50,61 +49,126 @@ func TestNewVirtualServicePreInstallPatch(t *testing.T) {
 	name := "test"
 	namespace := "test-namespace"
 	vs := []VirtualSvcMeta{{Name: name, Namespace: namespace}}
-
 	prefix := "-old"
-	expectedPatch := virtualServicePatch{Spec: specPatch{Hosts: []string{"my-domain-old.kyma.io"}}}
-
 	expectedCRPath := path.Join("/apis", "networking.istio.io/v1alpha3", "namespaces", namespace, "virtualservices", name)
 
-	p := NewVirtualServicePreInstallPatch(vs, prefix)
-	httpClient := restFake.CreateHTTPClient(func(request *http.Request) (*http.Response, error) {
-		require.NotNil(t, request)
-		switch request.Method {
-		case http.MethodGet:
-			{
-				assertGet(t, request, expectedCRPath)
-				resp := getVirtSvc(t)
-				return resp, nil
-			}
-		case http.MethodPatch:
-			{
-				assertPatch(t, request, expectedPatch)
-				resp := &http.Response{StatusCode: http.StatusNoContent}
-				return resp, nil
-			}
-		default:
-			{
-				return nil, errors.New(fmt.Sprintf("Not supported method: %s", request.Method))
-			}
-		}
-	})
-
-	restClient := &restFake.RESTClient{
-		NegotiatedSerializer: nil,
-		GroupVersion:         schema.GroupVersion{},
-		VersionedAPIPath:     "",
-		Err:                  nil,
-		Req:                  nil,
-		Client:               httpClient,
-		Resp:                 nil,
+	testCases := []struct {
+		Name           string
+		ExpectedPatch  virtualServicePatch
+		GetVirtSvcFn   func(t *testing.T) *http.Response
+		PatchVirtSvcFn func(t *testing.T) *http.Response
+		ExpectedError  error
+	}{
+		{
+			Name:           "Success",
+			ExpectedPatch:  virtualServicePatch{Spec: specPatch{Hosts: []string{"my-domain-old.kyma.io"}}},
+			GetVirtSvcFn:   createResponseFromTestFile,
+			PatchVirtSvcFn: createPatchResponse,
+			ExpectedError:  nil,
+		},
+		{
+			Name:          "Patching of virtual service failed",
+			ExpectedPatch: virtualServicePatch{Spec: specPatch{Hosts: []string{"my-domain-old.kyma.io"}}},
+			GetVirtSvcFn:  createResponseFromTestFile,
+			PatchVirtSvcFn: func(t *testing.T) *http.Response {
+				return &http.Response{StatusCode: http.StatusForbidden}
+			},
+			ExpectedError: errors.New("while patching virtual service"),
+		},
+		{
+			Name: "Getting of virtual service failed",
+			GetVirtSvcFn: func(t *testing.T) *http.Response {
+				return &http.Response{StatusCode: http.StatusForbidden}
+			},
+			PatchVirtSvcFn: createPatchResponse,
+			ExpectedError:  errors.New("while getting virtual service"),
+		},
+		{
+			Name:           "Host name is `test`",
+			GetVirtSvcFn:   getCreateMinimalResponseFn("test"),
+			ExpectedPatch:  virtualServicePatch{Spec: specPatch{Hosts: []string{"test-old"}}},
+			PatchVirtSvcFn: createPatchResponse,
+			ExpectedError:  nil,
+		},
+		//TODO: os such situation can show up?
+		{
+			Name:           "Host name is empty",
+			ExpectedPatch:  virtualServicePatch{Spec: specPatch{Hosts: []string{"-old"}}},
+			GetVirtSvcFn:   getCreateMinimalResponseFn(""),
+			PatchVirtSvcFn: createPatchResponse,
+			ExpectedError:  nil,
+		},
 	}
 
-	ctx := context.TODO()
-	//WHEN
-	err := p.patchVirtSvc(ctx, restClient, name, namespace)
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			p := NewVirtualServicePreInstallPatch(vs, prefix)
+			httpClient := restFake.CreateHTTPClient(func(request *http.Request) (*http.Response, error) {
+				require.NotNil(t, request)
+				switch request.Method {
+				case http.MethodGet:
+					{
+						assertGet(t, request, expectedCRPath)
+						resp := testCase.GetVirtSvcFn(t)
+						return resp, nil
+					}
+				case http.MethodPatch:
+					{
+						assertPatch(t, request, testCase.ExpectedPatch)
+						resp := testCase.PatchVirtSvcFn(t)
+						return resp, nil
+					}
+				default:
+					{
+						return nil, errors.New(fmt.Sprintf("Not supported method: %s", request.Method))
+					}
+				}
+			})
 
-	//THEN
-	require.NoError(t, err)
+			restClient := &restFake.RESTClient{Client: httpClient}
+
+			ctx := context.TODO()
+
+			//WHEN
+			err := p.patchVirtSvc(ctx, restClient, name, namespace)
+
+			//THEN
+			if testCase.ExpectedError == nil {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				assert.Contains(t, err.Error(), testCase.ExpectedError.Error())
+			}
+		})
+	}
+
 }
 
-func getVirtSvc(t *testing.T) *http.Response {
+func createResponseFromTestFile(t *testing.T) *http.Response {
 	out, err := ioutil.ReadFile("./test_files/virtual-service.yaml")
 	require.NoError(t, err)
-
 	reader := strings.NewReader(string(out))
 	body := ioutil.NopCloser(reader)
 	resp := &http.Response{Body: body}
 	resp.StatusCode = http.StatusOK
+	return resp
+}
+
+func getCreateMinimalResponseFn(host string) func(t *testing.T) *http.Response {
+	return func(t *testing.T) *http.Response {
+		virtSvc := virtSvc{Spec: virtSvcSpec{Hosts: []string{host}}}
+		out, err := json.Marshal(&virtSvc)
+		require.NoError(t, err)
+		reader := strings.NewReader(string(out))
+		body := ioutil.NopCloser(reader)
+		resp := &http.Response{Body: body}
+		resp.StatusCode = http.StatusOK
+		return resp
+	}
+}
+
+func createPatchResponse(t *testing.T) *http.Response {
+	resp := &http.Response{StatusCode: http.StatusNoContent}
 	return resp
 }
 
