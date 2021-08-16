85c0e0f6d7ec50a86cffab7ef0cce2697a4fafe2
Suleyman Akbas <suleyman.akbas@sap.com>
2021-08-16 14:49:26 +0200

    Revert renaming busola migrator (#103)
    * Revert renaming busola migrator
    
    * image bump

--- pkg/reconciler/instances/loader.go
+++ pkg/reconciler/instances/loader.go
@@ -5,8 +5,8 @@ package instances
 import (
 	//import required to register component reconciler 'base' in reconciler registry
 	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/base"
-	//import required to register component reconciler 'busola' in reconciler registry
-	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/busola"
+	//import required to register component reconciler 'busolamigrator' in reconciler registry
+	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/busolamigrator"
 	//import required to register component reconciler 'istio' in reconciler registry
 	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/istio"
 	//import required to register component reconciler 'simpletestcomponent' in reconciler registry
