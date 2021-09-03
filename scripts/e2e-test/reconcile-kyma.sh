ae1554f21cdf8ec87847425851d9905bdd1384f7
Muhammad Faizan <m.faizan@sap.com>
2021-09-03 15:17:10 +0200

    groomed script

--- scripts/e2e-test/reconcile-kyma.sh
+++ scripts/e2e-test/reconcile-kyma.sh
@@ -1,36 +1,76 @@
 #!/usr/bin/env sh
+
+# Description: Initiates Kyma reconciliation requests to reconciler and waits until Kyma is installed
+
+## ---------------------------------------------------------------------------------------
+## Configurations and Variables
+## ---------------------------------------------------------------------------------------
 set -e
-## Add curl and jq
-apk --no-cache add curl jq
 
-## Execute HTTP PUT to mothership-reconciler to http://mothership-reconciler-host:port/v1/clusters
-statusURL=$(curl --request POST -sL \
-     --url 'http://reconciler-mothership-reconciler.reconciler/v1/clusters'\
-     --data @/tmp/body.json | jq -r .statusUrl)
-echo "statusURL: ${statusURL}"
+readonly RECONCILER_HOST="http://reconciler-mothership-reconciler.reconciler"
+readonly RECONCILER_TIMEOUT=1200 # in secs
+readonly RECONCILER_DELAY=2 # in secs
 
-if [[ ! $statusURL ]]; then
-  echo "reconciliation request failed: statusURL is empty"
-  exit 1
-fi
+readonly RECONCILE_API="${RECONCILER_HOST}/v1/clusters"
+readonly RECONCILE_PAYLOAD_FILE="/tmp/body.json"
 
-## Wait until Kyma is installed
-timeout=1200 # in secs
-delay=2 # in secs
-iterationsLeft=$(( timeout/delay ))
-while : ; do
-  status=$(curl -sL http://$statusURL | jq -r .status)
-  if [ "${status}" = "ready" ]; then
-    echo "kyma is installed"
-    exit 0
-  fi
+## ---------------------------------------------------------------------------------------
+## Functions
+## ---------------------------------------------------------------------------------------
 
-  if [ "$timeout" -ne 0 ] && [ "$iterationsLeft" -le 0 ]; then
-    echo "timeout reached on kyma installation error. Exiting"
+# Waits until Kyma reconciliation is in ready state
+function wait_until_kyma_installed() {
+  iterationsLeft=$(( RECONCILER_TIMEOUT/RECONCILER_DELAY ))
+  while : ; do
+    status=$(curl -sL http://"$RECONCILE_STATUS_URL" | jq -r .status)
+    if [ "${status}" = "ready" ]; then
+      echo "Kyma is installed"
+      exit 0
+    fi
+
+    if [ "$RECONCILER_TIMEOUT" -ne 0 ] && [ "$iterationsLeft" -le 0 ]; then
+      echo "timeout reached on Kyma installation error. Exiting"
+      exit 1
+    fi
+
+    sleep $RECONCILER_DELAY
+    echo "waiting to get Kyma installed, current status: ${status} ...."
+    iterationsLeft=$(( iterationsLeft-1 ))
+  done
+}
+
+# Sends HTTP POST request to mothership-reconciler to trigger reconciliation of Kyma
+function send_reconciliation_request() {
+  echo "sending reconciliation request to mothership-reconciler at: ${RECONCILE_API}"
+  statusURL=$(curl --request POST -sL \
+       --url "${RECONCILE_API}"\
+       --data @"${RECONCILE_PAYLOAD_FILE}" | jq -r .statusUrl)
+
+  export RECONCILE_STATUS_URL="${statusURL}"
+}
+
+# Checks if the reconciler returned status url is valid or not
+function check_reconcile_status_url() {
+  echo "RECONCILE_STATUS_URL: ${RECONCILE_STATUS_URL}"
+  if [[ ! $RECONCILE_STATUS_URL ]]; then
+    echo "reconciliation request failed: RECONCILE_STATUS_URL is invalid"
     exit 1
   fi
+}
 
-  sleep $delay
-  echo "waiting to get Kyma installed, current status: ${status} ...."
-  iterationsLeft=$(( iterationsLeft-1 ))
-done
+## ---------------------------------------------------------------------------------------
+## Execution steps
+## ---------------------------------------------------------------------------------------
+
+# Install curl and jq
+echo "Installing curl and jq to the environment"
+apk --no-cache add curl jq
+
+# Send reconciliation http request to mothership-reconciler
+send_reconciliation_request
+
+# Check if reconcile status url is valid
+check_reconcile_status_url
+
+# Wait until Kyma is installed
+wait_until_kyma_installed
