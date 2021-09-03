eb65879503c8337f412a2367740884b9444cd895
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-09-03 13:06:43 +0200

    Show postgres isolation level during registry bootstrap

--- pkg/app/registry.go
+++ pkg/app/registry.go
@@ -32,6 +32,7 @@ func (or *ApplicationRegistry) init() error {
 	if or.initialized {
 		return nil
 	}
+
 	var err error
 	if or.logger, err = logger.NewLogger(or.debug); err != nil {
 		return err
@@ -43,8 +44,43 @@ func (or *ApplicationRegistry) init() error {
 		return err
 	}
 	or.initOperationsRegistry()
+
+	or.dumpPostgresIsolationLevel()
+
 	or.initialized = true
 	return nil
+}
+
+func (or *ApplicationRegistry) dumpPostgresIsolationLevel() {
+	//dump isolation level of Postgres
+	dbConn, err := or.connectionFactory.NewConnection()
+	if err != nil {
+		or.logger.Warnf("Not able to open DB connection to verify DB isolation level: %s", err)
+		return
+	}
+
+	defer func() {
+		if err := dbConn.Close(); err != nil {
+			or.logger.Warnf("Failed to close DB connection which was used to get Postgres isolation level: %s", err)
+		}
+	}()
+
+	if dbConn.Type() == db.Postgres {
+		res, err := dbConn.Query("SHOW TRANSACTION ISOLATION LEVEL")
+		if err == nil {
+			var isoLevel interface{}
+			if res.Next() {
+				if err := res.Scan(&isoLevel); err != nil {
+					or.logger.Infof("Failed to bind Postgres result including isolation level: %s", err)
+				}
+				or.logger.Infof("Postgres isolation level is: %v", isoLevel)
+			} else {
+				or.logger.Info("Postgres isolation level unknown")
+			}
+		} else {
+			or.logger.Warnf("Failed to get isolation level from Postgres DB: %s", err)
+		}
+	}
 }
 
 func (or *ApplicationRegistry) Close() error {
