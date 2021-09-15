e66f30ab2f86038756326b0c0ca90e21abcfc712
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-09-15 10:12:42 +0200

    Support different logger output formats

--- pkg/logger/zap.go
+++ pkg/logger/zap.go
@@ -6,27 +6,51 @@ import (
 	"os"
 )
 
+const (
+	OutputFormatJSON  OutputFormat = "json"
+	OutputFormatPlain OutputFormat = "plain"
+)
+
+var outputFormat = OutputFormatJSON
+
+type OutputFormat string
+
 func NewLogger(debug bool) *zap.SugaredLogger {
 	logLevel := zapcore.InfoLevel
 	if debug {
 		logLevel = zapcore.DebugLevel
 	}
+	return newLogger(logLevel).Sugar()
+}
 
-	logger := zap.New(
+func newLogger(logLevel zapcore.Level) *zap.Logger {
+	encoderConfig := zapcore.EncoderConfig{
+		MessageKey:   "message",
+		LevelKey:     "level",
+		EncodeLevel:  zapcore.CapitalLevelEncoder,
+		TimeKey:      "time",
+		EncodeTime:   zapcore.ISO8601TimeEncoder,
+		CallerKey:    "caller",
+		EncodeCaller: zapcore.ShortCallerEncoder,
+	}
+
+	var encoder zapcore.Encoder
+	switch outputFormat {
+	case OutputFormatPlain:
+		encoder = zapcore.NewConsoleEncoder(encoderConfig)
+	case OutputFormatJSON:
+		encoder = zapcore.NewJSONEncoder(encoderConfig)
+	}
+
+	return zap.New(
 		zapcore.NewCore(
-			zapcore.NewJSONEncoder(zapcore.EncoderConfig{
-				MessageKey:   "message",
-				LevelKey:     "level",
-				EncodeLevel:  zapcore.CapitalLevelEncoder,
-				TimeKey:      "time",
-				EncodeTime:   zapcore.ISO8601TimeEncoder,
-				CallerKey:    "caller",
-				EncodeCaller: zapcore.ShortCallerEncoder,
-			}),
-			zapcore.Lock(os.Stdout),
+			encoder,
+			zapcore.Lock(os.Stderr),
 			zap.NewAtomicLevelAt(logLevel),
 		),
 		zap.ErrorOutput(os.Stderr))
+}
 
-	return logger.Sugar()
+func SetOutputFormat(of OutputFormat) {
+	outputFormat = of
 }
