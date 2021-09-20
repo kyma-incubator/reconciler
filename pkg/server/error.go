ad3c908f4ad25a1feee06aeb55c7f61eb609d590
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-09-20 11:26:38 +0200

    Log error responses properly

--- pkg/server/error.go
+++ pkg/server/error.go
@@ -2,15 +2,29 @@ package server
 
 import (
 	"encoding/json"
-	"fmt"
+	"github.com/kyma-incubator/reconciler/pkg/logger"
+	"github.com/pkg/errors"
 	"net/http"
 )
 
 func SendHTTPError(w http.ResponseWriter, httpCode int, resp interface{}) {
-	w.WriteHeader(httpCode)
-	w.Header().Set("content-type", "application/json")
-	if err := json.NewEncoder(w).Encode(resp); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		http.Error(w, fmt.Sprintf("Failed to encode response payload to JSON: %s", err), http.StatusInternalServerError)
+	log := logger.NewLogger(false)
+
+	payload, err := json.Marshal(resp)
+	if err != nil {
+		err = errors.Wrap(err, "Failed to encode HTTP error response to JSON")
+	} else {
+		w.WriteHeader(httpCode)
+		w.Header().Set("content-type", "application/json")
+		if _, err = w.Write(payload); err == nil {
+			log.Infof("Sending HTTP error response (httpCode %d): %s", httpCode, payload)
+			return
+		}
+		err = errors.Wrap(err, "Failed to write error payload to HTTP response writer")
 	}
+
+	log.Errorf("Failed to process HTTP error response (fallback to %d - internal server error): %v",
+		http.StatusInternalServerError, err)
+	w.WriteHeader(http.StatusInternalServerError)
+	http.Error(w, err.Error(), http.StatusInternalServerError)
 }
