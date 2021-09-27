c9dc0b8ff396a69f51819bab9a44c6f71720e2f4
Damian Badura <45110612+dbadura@users.noreply.github.com>
2021-09-27 13:34:26 +0200

    Fix callback message model (#214)
    * fix callback message model
    
    * fix lint errors

--- cmd/reconciler/start/service/integration_test.go
+++ cmd/reconciler/start/service/integration_test.go
@@ -363,18 +363,10 @@ func newCallbackMock(t *testing.T) (*http.Server, chan *reconciler.CallbackMessa
 
 		t.Logf("Callback mock server received following callback request: %s", string(body))
 
-		callbackData := make(map[string]interface{})
+		callbackData := &reconciler.CallbackMessage{}
 		require.NoError(t, json.Unmarshal(body, &callbackData))
 
-		status, err := reconciler.NewStatus(fmt.Sprintf("%s", callbackData["status"]))
-		require.NoError(t, err)
-		callbackC <- &reconciler.CallbackMessage{
-			Status: status,
-			Error: func() *string {
-				errMsg := fmt.Sprintf("%v", callbackData["error"])
-				return &errMsg
-			}(),
-		}
+		callbackC <- callbackData
 	})
 
 	srv := &http.Server{
