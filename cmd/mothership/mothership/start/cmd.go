d1016e575682c2a99b3d08ac787df3bf676617f5
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-10-20 18:30:52 +0200

    Improve testing

--- cmd/mothership/mothership/start/cmd.go
+++ cmd/mothership/mothership/start/cmd.go
@@ -38,6 +38,7 @@ func NewCmd(o *Options) *cobra.Command {
 	cmd.Flags().IntVar(&o.Port, "server-port", 8080, "Webserver port")
 	cmd.Flags().StringVar(&o.SSLCrt, "server-crt", "", "Path to SSL certificate file")
 	cmd.Flags().StringVar(&o.SSLKey, "server-key", "", "Path to SSL key file")
+	cmd.Flags().IntVarP(&o.MaxParallelOperations, "max-parallel", "", 0, "Maximal parallel reconciled components per cluster, 0 means unlimited")
 	cmd.Flags().IntVarP(&o.Workers, "worker-count", "", 50, "Size of the reconciler worker pool")
 	cmd.Flags().DurationVarP(&o.OrphanOperationTimeout, "orphan-timeout", "", 10*time.Minute, "Timeout until a processed operation which hasn't received status updates from its worker will be restarted")
 	cmd.Flags().DurationVarP(&o.WatchInterval, "watch-interval", "", 1*time.Minute, "Size of the reconciler worker pool")
