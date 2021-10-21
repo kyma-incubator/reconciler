dc97bffcd4527cb79a5ef332b22605e784f4c2a8
rJankowski93 <rafal.jankowski.93@gmail.com>
2021-10-21 10:44:06 +0200

    Enhance error responses to KEB

--- cmd/mothership/mothership/start/http.go
+++ cmd/mothership/mothership/start/http.go
@@ -628,22 +628,24 @@ func newClusterResponse(r *http.Request, clusterState *cluster.State, reconcilia
 	}
 
 	var failures []keb.Failure
-	reconciliations, err := reconciliationRepository.GetReconciliations(&reconciliation.WithRuntimeID{RuntimeID: clusterState.Cluster.RuntimeID})
-	if err != nil {
-		return nil, err
-	}
-	if len(reconciliations) > 0 {
-		operations, err := reconciliationRepository.GetOperations(reconciliations[0].SchedulingID)
+	if clusterState.Status.Status == model.ClusterStatusError || clusterState.Status.Status == model.ClusterStatusReconcileFailed || clusterState.Status.Status == model.ClusterStatusReconciling {
+		reconciliations, err := reconciliationRepository.GetReconciliations(&reconciliation.WithClusterConfigStatus{ClusterConfigStatus: clusterState.Status.ID})
 		if err != nil {
 			return nil, err
 		}
+		if len(reconciliations) > 0 {
+			operations, err := reconciliationRepository.GetOperations(reconciliations[0].SchedulingID)
+			if err != nil {
+				return nil, err
+			}
 
-		for _, operation := range operations {
-			if operation.State.IsError() {
-				failures = append(failures, keb.Failure{
-					Component: operation.Component,
-					Reason:    operation.Reason,
-				})
+			for _, operation := range operations {
+				if operation.State.IsError() {
+					failures = append(failures, keb.Failure{
+						Component: operation.Component,
+						Reason:    operation.Reason,
+					})
+				}
 			}
 		}
 	}
