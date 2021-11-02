b5ed043645c51ea232b248d1a2a65ed4886b1f37
Jan Wozniak <wozniak.jan@gmail.com>
2021-11-02 08:26:35 +0100

    [gen] sc-migration reconciler
    ./pkg/reconciler/instances/reconcilerctl.sh add sc-migration

---
+++ pkg/reconciler/instances/scmigration/action.go
@@ -0,0 +1,20 @@
+package scmigration
+
+import (
+	"github.com/kyma-incubator/reconciler/pkg/reconciler/service"
+)
+
+//TODO: please implement component specific action logic here
+type CustomAction struct {
+	name string
+}
+
+func (a *CustomAction) Run(context *service.ActionContext) error {
+	if _, err := context.KubeClient.Clientset(); err != nil { //scmigration how to retrieve native Kubernetes GO client
+		context.Logger.Errorf("Failed to retrieve native Kubernetes GO client")
+	}
+
+	context.Logger.Infof("Action '%s' executed (passed version was '%s')", a.name, context.Task.Version)
+
+	return nil
+}
---
+++ pkg/reconciler/instances/scmigration/scmigration.go
@@ -0,0 +1,38 @@
+package scmigration
+
+import (
+	"github.com/kyma-incubator/reconciler/pkg/logger"
+	"github.com/kyma-incubator/reconciler/pkg/reconciler/service"
+)
+
+const ReconcilerName = "sc-migration"
+
+//nolint:gochecknoinits //usage of init() is intended to register reconciler-instances in centralized registry
+func init() {
+	log := logger.NewLogger(false)
+
+	log.Debugf("Initializing component reconciler '%s'", ReconcilerName)
+	reconciler, err := service.NewComponentReconciler(ReconcilerName)
+	if err != nil {
+		log.Fatalf("Could not create '%s' component reconciler: %s", ReconcilerName, err)
+	}
+
+	//TODO: please configure the component reconciler for your component by setting dependencies and custom actions
+	//configure reconciler
+	reconciler.
+		//list dependencies (these components have to be available before this component reconciler is able to run)
+		WithDependencies("componentX", "componentY", "componentZ").
+		//register reconciler pre-action (executed BEFORE reconciliation happens)
+		WithPreReconcileAction(&CustomAction{
+			name: "pre-action",
+		}).
+		//register reconciler action (custom reconciliation logic). If no custom reconciliation action is provided,
+		//the default reconciliation logic provided by reconciler-framework will be used.
+		WithReconcileAction(&CustomAction{
+			name: "install-action",
+		}).
+		//register reconciler post-action (executed AFTER reconciliation happened)
+		WithPostReconcileAction(&CustomAction{
+			name: "post-action",
+		})
+}
