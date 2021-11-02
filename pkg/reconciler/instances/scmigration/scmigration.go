0d754ebc5b44a28790783afcf840ce3d29e3ac5b
Jan Wozniak <wozniak.jan@gmail.com>
2021-11-02 09:19:47 +0100

    Introduce btp-operator-migration code as reconciler
    https://github.com/SAP/sap-btp-service-operator-migration

--- pkg/reconciler/instances/scmigration/scmigration.go
+++ pkg/reconciler/instances/scmigration/scmigration.go
@@ -7,7 +7,7 @@ import (
 
 const ReconcilerName = "sc-migration"
 
-//nolint:gochecknoinits //usage of init() is intended to register reconciler-instances in centralized registry
+//nolint:gochecknoinits
 func init() {
 	log := logger.NewLogger(false)
 
@@ -17,22 +17,5 @@ func init() {
 		log.Fatalf("Could not create '%s' component reconciler: %s", ReconcilerName, err)
 	}
 
-	//TODO: please configure the component reconciler for your component by setting dependencies and custom actions
-	//configure reconciler
-	reconciler.
-		//list dependencies (these components have to be available before this component reconciler is able to run)
-		WithDependencies("componentX", "componentY", "componentZ").
-		//register reconciler pre-action (executed BEFORE reconciliation happens)
-		WithPreReconcileAction(&CustomAction{
-			name: "pre-action",
-		}).
-		//register reconciler action (custom reconciliation logic). If no custom reconciliation action is provided,
-		//the default reconciliation logic provided by reconciler-framework will be used.
-		WithReconcileAction(&CustomAction{
-			name: "install-action",
-		}).
-		//register reconciler post-action (executed AFTER reconciliation happened)
-		WithPostReconcileAction(&CustomAction{
-			name: "post-action",
-		})
+	reconciler.WithReconcileAction(&reconcileAction{})
 }
