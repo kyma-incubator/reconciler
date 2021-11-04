67ffe92a698ea9c8f1f4e927228d6a3cbb01694b
Patryk Strugacz <werdes72@users.noreply.github.com>
2021-11-04 08:33:37 +0100

    Ory upgrade and config migration (#289)
    * Add preaction ory secret check
    
    * Add initial update logic
    
    * Add more ory upgrade logic, update tests
    
    * Minor fixes
    
    * Log messages, obsolete return statements
    
    * Fix bugs
    
    * Fix ctx param
    
    * Log information about hydra rollout
    
    * Add new model field, CR fixes
    
    * CR refactor and fixes
    
    * Get rid of ensureOrySecret function, fix test
    
    * Add test
    
    * More tests
    
    * Add proper action tests for config update scenario
    
    * Add ory db config update tests
    
    * Go mod tidy
    
    * Add more ory upgrade logic, update tests
    
    * Add test
    
    * Add proper action tests for config update scenario
    
    * Add ory db config update tests
    
    * Rebase

--- pkg/reconciler/instances/ory/db/db_test.go
+++ pkg/reconciler/instances/ory/db/db_test.go
@@ -5,7 +5,10 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zaptest"
 	"gopkg.in/yaml.v3"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
 
@@ -85,6 +88,8 @@ const (
 )
 
 func TestDBSecret(t *testing.T) {
+	logger := zaptest.NewLogger(t).Sugar()
+
 	t.Run("Deployment with Postgres SQL in cluster", func(t *testing.T) {
 		// given
 		t.Parallel()
@@ -95,7 +100,7 @@ func TestDBSecret(t *testing.T) {
 		dsnExpected := cfg.preparePostgresDSN()
 
 		// when
-		secret, errGet := Get(name, values)
+		secret, errGet := Get(name, values, logger)
 
 		// then
 		require.NoError(t, errNew)
@@ -115,7 +120,7 @@ func TestDBSecret(t *testing.T) {
 		dsnExpected := cfg.prepareGenericDSN()
 
 		// when
-		secret, errGet := Get(name, values)
+		secret, errGet := Get(name, values, logger)
 
 		// then
 		require.NoError(t, errNew)
@@ -135,7 +140,7 @@ func TestDBSecret(t *testing.T) {
 		dsnExpected := cfg.prepareMySQLDSN()
 
 		// when
-		secret, errGet := Get(name, values)
+		secret, errGet := Get(name, values, logger)
 
 		// then
 		require.NoError(t, errNew)
@@ -154,7 +159,7 @@ func TestDBSecret(t *testing.T) {
 		dsnExpected := cfg.prepareGenericDSN()
 
 		// when
-		secret, errGet := Get(name, values)
+		secret, errGet := Get(name, values, logger)
 
 		// then
 		require.NoError(t, errNew)
@@ -171,11 +176,11 @@ func TestDBSecret(t *testing.T) {
 		require.NoError(t, err)
 
 		// when
-		secret, err := Get(name, values)
+		secret, err := Get(name, values, logger)
 
 		// then
 		require.NoError(t, err)
-		assert.Equal(t, secret.StringData["dsn"], "memory")
+		assert.Equal(t, "memory", secret.StringData["dsn"])
 	})
 
 	t.Run("Deployment with yaml values error", func(t *testing.T) {
@@ -184,7 +189,221 @@ func TestDBSecret(t *testing.T) {
 		assert.Nil(t, values)
 		assert.Error(t, err)
 	})
+}
 
+func TestDBSecretUpdate(t *testing.T) {
+	logger := zaptest.NewLogger(t).Sugar()
+
+	t.Run("Existing secret with memory persistence should not be updated with the same config", func(t *testing.T) {
+		// given
+		t.Parallel()
+		values, err := unmarshalTestValues(noDBYaml)
+		require.NoError(t, err)
+		oldSecret := fixSecretWithNameDsn("test-memory-secret", "memory")
+
+		// when
+		secretData, errUpdate := Update(values, oldSecret, logger)
+
+		// then
+		require.NoError(t, errUpdate)
+		assert.Equal(t, 0, len(secretData))
+	})
+
+	t.Run("Existing secret with postgres persistence should not be updated with the same config", func(t *testing.T) {
+		// given
+		t.Parallel()
+		values, err := unmarshalTestValues(postgresYaml)
+		require.NoError(t, err)
+		oldSecret := fixSecretWithNameDsn("test-postgres-secret",
+			"postgres://hydra:secretpw@ory-postgresql.kyma-system.svc.cluster.local:5432/db4hydra?sslmode=disable&max_conn_lifetime=10s")
+
+		// when
+		secretData, errUpdate := Update(values, oldSecret, logger)
+
+		// then
+		require.NoError(t, errUpdate)
+		assert.Equal(t, 0, len(secretData))
+	})
+
+	t.Run("Existing secret with gcloud persistence should not be updated with the same config", func(t *testing.T) {
+		// given
+		t.Parallel()
+		values, err := unmarshalTestValues(gcloudYaml)
+		require.NoError(t, err)
+		oldSecret := fixSecretWithNameDsn("test-postgres-secret",
+			"postgres://hydra:secretpw@ory-gcloud-sqlproxy.kyma-system:5432/db4hydra?sslmode=disable&max_conn_lifetime=10s")
+
+		// when
+		secretData, errUpdate := Update(values, oldSecret, logger)
+
+		// then
+		require.NoError(t, errUpdate)
+		assert.Equal(t, 0, len(secretData))
+	})
+
+	t.Run("Existing secret with mysql persistence should not be updated with the same config", func(t *testing.T) {
+		// given
+		t.Parallel()
+		values, err := unmarshalTestValues(mysqlDBYaml)
+		require.NoError(t, err)
+		oldSecret := fixSecretWithNameDsn("test-postgres-secret",
+			"mysql://hydra:secretpw@tcp(mydb.mynamespace:1234)/db4hydra?parseTime=true&max_conn_lifetime=10s")
+
+		// when
+		secretData, errUpdate := Update(values, oldSecret, logger)
+
+		// then
+		require.NoError(t, errUpdate)
+		assert.Equal(t, 0, len(secretData))
+	})
+
+	t.Run("Existing secret with custom persistence should not be updated with the same config", func(t *testing.T) {
+		// given
+		t.Parallel()
+		values, err := unmarshalTestValues(customDBYaml)
+		require.NoError(t, err)
+		oldSecret := fixSecretWithNameDsn("test-postgres-secret",
+			"cockroach://hydra:secretpw@mydb.mynamespace:1234/db4hydra?sslmode=disable&max_conn_lifetime=10s")
+
+		// when
+		secretData, errUpdate := Update(values, oldSecret, logger)
+
+		// then
+		require.NoError(t, errUpdate)
+		assert.Equal(t, 0, len(secretData))
+	})
+
+	t.Run("Existing secret with memory persistence should be updated with postgres config", func(t *testing.T) {
+		// given
+		t.Parallel()
+		values, err := unmarshalTestValues(postgresYaml)
+		require.NoError(t, err)
+		oldSecret := fixSecretWithNameDsn("test-memory-secret", "memory")
+
+		// when
+		secretData, errUpdate := Update(values, oldSecret, logger)
+
+		// then
+		require.NoError(t, errUpdate)
+		assert.NotEqual(t, 0, len(secretData))
+		assert.Contains(t, secretData, "postgresql-password")
+		assert.Contains(t, secretData["dsn"], postgresURL)
+	})
+
+	t.Run("Existing secret with memory persistence should be updated with gcloud config", func(t *testing.T) {
+		// given
+		t.Parallel()
+		values, err := unmarshalTestValues(gcloudYaml)
+		require.NoError(t, err)
+		oldSecret := fixSecretWithNameDsn("test-memory-secret", "memory")
+
+		// when
+		secretData, errUpdate := Update(values, oldSecret, logger)
+
+		// then
+		require.NoError(t, errUpdate)
+		assert.NotEqual(t, 0, len(secretData))
+		assert.Contains(t, secretData, "gcp-sa.json")
+		assert.Contains(t, secretData["dsn"], "postgres")
+		assert.Contains(t, secretData["dsn"], "ory-gcloud-sqlproxy.kyma-system:5432")
+		assert.Contains(t, secretData["dsn"], "db4hydra")
+		assert.Contains(t, secretData["dsn"], dsnOpts)
+	})
+
+	t.Run("Existing secret with memory persistence should be updated with mysql config", func(t *testing.T) {
+		// given
+		t.Parallel()
+		values, err := unmarshalTestValues(mysqlDBYaml)
+		require.NoError(t, err)
+		oldSecret := fixSecretWithNameDsn("test-memory-secret", "memory")
+
+		// when
+		secretData, errUpdate := Update(values, oldSecret, logger)
+
+		// then
+		require.NoError(t, errUpdate)
+		assert.NotEqual(t, 0, len(secretData))
+		assert.Contains(t, secretData["dsn"], "mysql")
+		assert.Contains(t, secretData["dsn"], "mydb.mynamespace:1234")
+		assert.Contains(t, secretData["dsn"], "db4hydra")
+		assert.Contains(t, secretData["dsn"], mysqlDsnOpts)
+	})
+
+	t.Run("Existing secret with memory persistence should be updated with custom db config", func(t *testing.T) {
+		// given
+		t.Parallel()
+		values, err := unmarshalTestValues(customDBYaml)
+		require.NoError(t, err)
+		oldSecret := fixSecretWithNameDsn("test-memory-secret", "memory")
+
+		// when
+		secretData, errUpdate := Update(values, oldSecret, logger)
+
+		// then
+		require.NoError(t, errUpdate)
+		assert.NotEqual(t, 0, len(secretData))
+		assert.Contains(t, secretData["dsn"], "cockroach")
+		assert.Contains(t, secretData["dsn"], "mydb.mynamespace:1234")
+		assert.Contains(t, secretData["dsn"], "db4hydra")
+		assert.Contains(t, secretData["dsn"], dsnOpts)
+	})
+
+	t.Run("Existing secret with postgresql persistence should be updated with memory config", func(t *testing.T) {
+		// given
+		t.Parallel()
+		values, err := unmarshalTestValues(noDBYaml)
+		require.NoError(t, err)
+		oldSecret := fixSecretWithNameDsn("test-postgres-secret",
+			"postgres://hydra:pass@ory-postgresql.kyma-system.svc.cluster.local:5432/db4hydra?sslmode=disable&max_conn_lifetime=10s")
+
+		// when
+		secretData, errUpdate := Update(values, oldSecret, logger)
+
+		// then
+		require.NoError(t, errUpdate)
+		assert.NotEqual(t, 0, len(secretData))
+		assert.Contains(t, secretData["dsn"], "memory")
+	})
+
+	t.Run("Existing secret with postgresql persistence should be updated with mysql config", func(t *testing.T) {
+		// given
+		t.Parallel()
+		values, err := unmarshalTestValues(mysqlDBYaml)
+		require.NoError(t, err)
+		oldSecret := fixSecretWithNameDsn("test-postgres-secret",
+			"postgres://hydra:pass@ory-postgresql.kyma-system.svc.cluster.local:5432/db4hydra?sslmode=disable&max_conn_lifetime=10s")
+
+		// when
+		secretData, errUpdate := Update(values, oldSecret, logger)
+
+		// then
+		require.NoError(t, errUpdate)
+		assert.NotEqual(t, 0, len(secretData))
+		assert.Contains(t, secretData["dsn"], "mysql")
+		assert.Contains(t, secretData["dsn"], "mydb.mynamespace:1234")
+		assert.Contains(t, secretData["dsn"], "db4hydra")
+		assert.Contains(t, secretData["dsn"], mysqlDsnOpts)
+	})
+
+	t.Run("Existing secret with gcloud persistence should be updated with custom db config", func(t *testing.T) {
+		// given
+		t.Parallel()
+		values, err := unmarshalTestValues(customDBYaml)
+		require.NoError(t, err)
+		oldSecret := fixSecretWithNameDsn("test-postgres-secret",
+			"postgres://hydra:secretpw@ory-gcloud-sqlproxy.kyma-system:5432/db4hydra?sslmode=disable&max_conn_lifetime=10s")
+
+		// when
+		secretData, errUpdate := Update(values, oldSecret, logger)
+
+		// then
+		require.NoError(t, errUpdate)
+		assert.NotEqual(t, 0, len(secretData))
+		assert.Contains(t, secretData["dsn"], "cockroach")
+		assert.Contains(t, secretData["dsn"], "mydb.mynamespace:1234")
+		assert.Contains(t, secretData["dsn"], "db4hydra")
+		assert.Contains(t, secretData["dsn"], dsnOpts)
+	})
 }
 
 func unmarshalTestValues(yamlValues string) (map[string]interface{}, error) {
@@ -194,4 +413,19 @@ func unmarshalTestValues(yamlValues string) (map[string]interface{}, error) {
 		return nil, err
 	}
 	return values, nil
+}
+
+func fixSecretWithNameDsn(name, dsn string) *v1.Secret {
+	secret := v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: "test",
+		},
+		Data: map[string][]byte{
+			"dsn":           []byte(dsn),
+			"secretsCookie": []byte("somesecretcookie"),
+			"secretsSystem": []byte("somesecretsystem"),
+		},
+	}
+	return &secret
 }
