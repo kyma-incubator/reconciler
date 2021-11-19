5c6ee143ce59c53336763a9290e57cedd082b3cb
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-11-19 18:08:14 +0100

    Fix missing encryption flag in DB column

--- pkg/db/postgres.go
+++ pkg/db/postgres.go
@@ -2,10 +2,12 @@ package db
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
-
 	log "github.com/kyma-incubator/reconciler/pkg/logger"
+	"github.com/kyma-incubator/reconciler/pkg/model"
 	"github.com/pkg/errors"
+	"time"
 
 	//add Postgres driver:
 	_ "github.com/lib/pq"
@@ -190,6 +192,7 @@ func (pcf *postgresConnectionFactory) checkPostgresIsolationLevel() error {
 
 func (pcf *postgresConnectionFactory) migrateDatabase() error {
 	logger := log.NewLogger(pcf.debug)
+
 	dbConn, err := pcf.NewConnection()
 	if err != nil {
 		return errors.Wrap(err, "not able to open DB connection to perform migration")
@@ -199,17 +202,117 @@ func (pcf *postgresConnectionFactory) migrateDatabase() error {
 			logger.Warnf("Failed to close DB connection which was used to perform migration: %s", err)
 		}
 	}()
+
 	driver, err := postgres.WithInstance(dbConn.DB(), &postgres.Config{})
 	if err != nil {
 		return errors.Wrap(err, "not able to instantiate postgres driver for migration")
 	}
+
 	m, err := migrate.NewWithDatabaseInstance("file://"+pcf.migrationsDir, "postgres", driver)
 	if err != nil {
-		return errors.Wrap(err, "not able to instantiate migrator with database instance")
+		return errors.Wrap(err, "not able to instantiate DB migrator with database instance")
 	}
-	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		return errors.Wrapf(err, "not able to execute migrations: %s", err)
+	defer func() {
+		if srcErr, dbErr := m.Close(); srcErr != nil || dbErr != nil {
+			logger.Warnf("Failed to close DB migration instance properly: srcErr=%s | dbErr=%s", srcErr, dbErr)
+		}
+	}()
+
+	version, versionHasErr, err := m.Version()
+	if err != nil {
+		return errors.Wrap(err, "DB migration returned and error when retrieving current database schema version")
 	}
-	logger.Info("Database migrated")
+
+	if versionHasErr {
+		logger.Warnf("DB schema version %d has errors: retrying it", version)
+		version = version - 1 //if current version has an error,
+	}
+
+	for {
+		nextVersion := version + 1
+		err := m.Migrate(nextVersion)
+		switch err {
+		case nil:
+			logger.Debugf("DB migration to schema version %d finished", version)
+			if err := pcf.runMigrationStep(nextVersion, logger); err != nil {
+				return errors.Wrapf(err, "failed to execute migration steps for schema version %d", nextVersion)
+			}
+		case migrate.ErrNoChange:
+			logger.Infof("Migration finished")
+			break
+		default:
+			return errors.Wrap(err, "migration of data schema failed")
+		}
+	}
+}
+
+func (pcf *postgresConnectionFactory) runMigrationStep(version uint, logger *zap.SugaredLogger) error {
+	stepExecuted := true
+	startTime := time.Now()
+	switch version {
+	default:
+		stepExecuted = false
+	case 6:
+		//encrypt cluster-configuration column
+		conn, err := pcf.NewConnection()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := conn.Close(); err != nil {
+				logger.Warnf("Failed to close DB connection used for DB migration logic "+
+					"(related to schema version %d)", version)
+			}
+		}()
+
+		clCfgEntity := &model.ClusterConfigurationEntity{}
+		colHandler, err := NewColumnHandler(clCfgEntity, conn, logger)
+		if err != nil {
+			return errors.Wrap(err, "failed to get column handler for cluster configuration entity")
+		}
+		colVersion, err := colHandler.ColumnName("Version")
+		if err != nil {
+			return err
+		}
+		colComps, err := colHandler.ColumnName("Components")
+		if err != nil {
+			return err
+		}
+		res, err := conn.Query(fmt.Sprintf("SELECT %s, %s FROM %s", colVersion, colComps, clCfgEntity.Table()))
+		if err != nil {
+			return errors.Wrap(err, "failed to retrieve cluster configuration rows from DB")
+		}
+
+		var clusterConfigVersion int64
+		var compJSON string
+
+		enc, err := NewEncryptor(pcf.encryptionKey)
+		if err != nil {
+			return err
+		}
+		for res.Next() {
+			if err := res.Scan(clusterConfigVersion, compJSON); err != nil {
+				return err
+			}
+			if !json.Valid([]byte(compJSON)) {
+				logger.Debugf("Encrypting cluster-components for cluster-config with version '%d'", clusterConfigVersion)
+				//string is still a plain JSON - encrypt it
+				encCompJSON, err := enc.Encrypt(compJSON)
+				if err != nil {
+					return err
+				}
+				_, err = conn.Exec(fmt.Sprintf("UPDATE %s SET (%s=$1) WHERE %s=$2",
+					clCfgEntity.Table(), colComps, colVersion), encCompJSON, clusterConfigVersion)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	if stepExecuted {
+		logger.Infof("Execution of migation logic for schema version %d finished (took %.2f sec)",
+			version, time.Until(startTime).Seconds())
+	}
 	return nil
 }
