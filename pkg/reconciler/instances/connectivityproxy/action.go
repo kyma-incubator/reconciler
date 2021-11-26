49bc0a714c6737d5f9ca1c41d1dfbe5dd3cdee8e
ralikio <74771103+ralikio@users.noreply.github.com>
2021-11-26 12:37:47 +0100

    NIL Exception Fix

--- pkg/reconciler/instances/connectivityproxy/action.go
+++ pkg/reconciler/instances/connectivityproxy/action.go
@@ -54,7 +54,7 @@ func (a *CustomAction) Run(context *service.ActionContext) error {
 	if (binding != nil && app == nil) || (binding != nil && app != nil) {
 		context.Logger.Info("Reading secret")
 		bindingSecret, err := a.Loader.FindSecret(context, binding)
-		if err != nil {
+		if err != nil || bindingSecret == nil {
 			return errors.Wrap(err, "Error while retrieving secret")
 		}
 
