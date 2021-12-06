50db2680b2de51bf366e9d2d53381fa5362f2f0b
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-12-06 22:04:32 +0100

    Improve unit test

--- pkg/reconciler/kubernetes/internal/kubeclient_test.go
+++ pkg/reconciler/kubernetes/internal/kubeclient_test.go
@@ -3,8 +3,12 @@ package internal
 import (
 	"context"
 	"fmt"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
@@ -44,7 +48,8 @@ func TestSetNamespaceIfScoped(t *testing.T) {
 }
 
 const (
-	namespace = "kubeclient-test"
+	namespace          = "kubeclient-test"
+	containerBaseImage = "alpine"
 )
 
 func TestPatchReplace(t *testing.T) {
@@ -65,16 +70,22 @@ func TestPatchReplace(t *testing.T) {
 		"manifest-after.yaml"} {
 		unstructs, err := ToUnstructured(readFile(t, filepath.Join("test", manifest)), true)
 		require.NoError(t, err)
+
+		var expectedImage = containerBaseImage
+		if idxManifest%2 == 1 { //each second update updates the image from 'alpine' to 'alpine:3.14'
+			expectedImage = fmt.Sprintf("%s:3.14", expectedImage)
+		}
+
 		for idxUnstruct, unstruct := range unstructs {
 			t.Run(
 				fmt.Sprintf("Applying %s", unstruct.GetName()),
-				newTestFunc(kubeClient, unstruct, fmt.Sprintf("%d_%d", idxManifest, idxUnstruct)))
+				newTestFunc(kubeClient, unstruct, fmt.Sprintf("%d_%d", idxManifest, idxUnstruct), expectedImage))
 		}
 	}
 
 }
 
-func newTestFunc(kubeClient *KubeClient, unstruct *unstructured.Unstructured, label string) func(t *testing.T) {
+func newTestFunc(kubeClient *KubeClient, unstruct *unstructured.Unstructured, label, expectedImage string) func(t *testing.T) {
 	return func(t *testing.T) {
 		t.Logf("Applying %s and setting label %s", unstruct.GetKind(), label)
 		unstruct.SetLabels(map[string]string{"applied": label})
@@ -83,10 +94,44 @@ func newTestFunc(kubeClient *KubeClient, unstruct *unstructured.Unstructured, la
 		require.NoError(t, err)
 
 		k8sResourceUnstruct, err := kubeClient.Get(unstruct.GetKind(), unstruct.GetName(), unstruct.GetNamespace())
-
 		require.NoError(t, err)
+
+		//verify label
 		require.Equal(t, label, k8sResourceUnstruct.GetLabels()["applied"])
+
+		//verify image
+		switch strings.ToLower(k8sResourceUnstruct.GetKind()) {
+		case "statefulset":
+			var statefulSet v1.StatefulSet
+			err = runtime.DefaultUnstructuredConverter.
+				FromUnstructured(unstruct.UnstructuredContent(), &statefulSet)
+			require.NoError(t, err)
+			for _, container := range statefulSet.Spec.Template.Spec.Containers {
+				if requireImageName(t, container, expectedImage) {
+					return
+				}
+			}
+		case "deployment":
+			var deployment v1.Deployment
+			err = runtime.DefaultUnstructuredConverter.
+				FromUnstructured(unstruct.UnstructuredContent(), &deployment)
+			require.NoError(t, err)
+			for _, container := range deployment.Spec.Template.Spec.Containers {
+				if requireImageName(t, container, expectedImage) {
+					return
+				}
+			}
+		}
 	}
+}
+
+func requireImageName(t *testing.T, container corev1.Container, expectedImage string) bool {
+	if strings.HasPrefix(container.Image, containerBaseImage) {
+		t.Logf("Found container image '%s'", expectedImage)
+		require.Equal(t, expectedImage, container.Image)
+		return true
+	}
+	return false
 }
 
 func deleteNamespace(t *testing.T, kubeClient *KubeClient, ignoreError bool) {
