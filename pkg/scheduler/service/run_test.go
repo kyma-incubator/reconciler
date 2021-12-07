f39d048be2b67cbd048c5b7f684420b00f9f9bc2
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-12-07 10:58:04 +0100

    Increase timeouts to avoid false-positives while cleaner bug exists (#534)

--- pkg/scheduler/service/run_test.go
+++ pkg/scheduler/service/run_test.go
@@ -136,7 +136,7 @@ func runRemote(t *testing.T, expectedClusterStatus model.Status, timeout time.Du
 	})
 	remoteRunner.WithCleanerConfig(&CleanerConfig{
 		PurgeEntitiesOlderThan: 15 * time.Second,
-		CleanerInterval:        10 * time.Second,
+		CleanerInterval:        15 * time.Second,
 	})
 
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
@@ -155,7 +155,7 @@ func runRemote(t *testing.T, expectedClusterStatus model.Status, timeout time.Du
 	require.Equal(t, 1, getEntityLen(t, dbConn, &model.ReconciliationEntity{}))
 	require.Equal(t, 2, getEntityLen(t, dbConn, &model.OperationEntity{}))
 
-	time.Sleep(10 * time.Second) //give the cleaner some time to remove old entities
+	time.Sleep(15 * time.Second) //give the cleaner some time to remove old entities
 
 	require.NoError(t, err)
 	require.Equal(t, 0, getEntityLen(t, dbConn, &model.ReconciliationEntity{}))
