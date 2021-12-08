96b64acdbac320ffdddef8d979dcdd43d61f8a0a
Jeremy Harisch <jeremy.harisch@sap.com>
2021-12-08 11:00:06 +0100

    New bk tasks tests, incl fixes

--- pkg/scheduler/service/bookkeepingtask_test.go
+++ pkg/scheduler/service/bookkeepingtask_test.go
@@ -14,6 +14,91 @@ import (
 	"time"
 )
 
+func TestBookkeepingtask(t *testing.T) {
+	tests := []struct {
+		name        string
+		markOpsDone bool
+		customFunc  func(transition *ClusterStatusTransition) BookkeepingTask
+		expectedStatus model.OperationState
+	}{
+		{name: "Mark operations as orphan", markOpsDone: false, customFunc: func(transition *ClusterStatusTransition) BookkeepingTask {
+			return markOrphanOperation{
+				transition: transition,
+				logger:     logger.NewLogger(true),
+			}
+		}, expectedStatus: model.OperationStateOrphan},
+		{name: "Finish operations", markOpsDone: true, customFunc: func(transition *ClusterStatusTransition) BookkeepingTask {
+			return finishOperation{
+				transition: transition,
+				logger:     logger.NewLogger(true),
+			}
+		}, expectedStatus: model.OperationStateDone},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+
+			//create mock database connection
+			dbConn := db.NewTestConnection(t)
+			//prepare inventory
+			inventory, err := cluster.NewInventory(dbConn, true, cluster.MetricsCollectorMock{})
+			require.NoError(t, err)
+
+			//add cluster to inventory
+			clusterState, err := inventory.CreateOrUpdate(1, test.NewCluster(t, strconv.Itoa(1), 1, false, test.OneComponentDummy))
+			require.NoError(t, err)
+
+			//trigger reconciliation for cluster
+			reconRepo, err := reconciliation.NewPersistedReconciliationRepository(dbConn, true)
+			require.NoError(t, err)
+			reconEntity, err := reconRepo.CreateReconciliation(clusterState, nil)
+			require.NoError(t, err)
+			require.NotEmpty(t, reconEntity.Lock)
+			require.False(t, reconEntity.Finished)
+
+			//mark all operations to be done, if needed for tc
+			if tc.markOpsDone {
+				opEntities, err := reconRepo.GetOperations(reconEntity.SchedulingID)
+				require.NoError(t, err)
+				for _, opEntity := range opEntities {
+					err := reconRepo.UpdateOperationState(opEntity.SchedulingID, opEntity.CorrelationID, model.OperationStateDone, true)
+					require.NoError(t, err)
+				}
+			}
+
+			//setup bookkeeper task
+			transition := newClusterStatusTransition(dbConn, inventory, reconRepo, logger.NewLogger(true))
+
+			//initialize bookkeeper
+			bk := newBookkeeper(
+				reconRepo,
+				&BookkeeperConfig{
+					OperationsWatchInterval: 100 * time.Millisecond,
+					OrphanOperationTimeout:  1 * time.Microsecond,
+					MaxRetries:              150,
+				},
+				logger.NewLogger(true),
+			)
+
+			//setup reconciliation result
+			reconResult := getReconResult(t, reconRepo, bk)
+
+			//execute bookkeepingtask
+			errSlice := tc.customFunc(transition).Apply(reconResult, bk.config)
+			for _, e := range errSlice {
+				require.NoError(t, e)
+			}
+
+			//check correct status
+			reconResult = getReconResult(t, reconRepo, bk)
+			operations := reconResult.GetOperations()
+			for _, o := range operations {
+				require.Equal(t,tc.expectedStatus, o.State)
+			}
+		})
+	}
+}
+
 func TestBookkeepingtaskParallel(t *testing.T) {
 	tests := []struct {
 		name        string
@@ -21,20 +106,20 @@ func TestBookkeepingtaskParallel(t *testing.T) {
 		customFunc  func(transition *ClusterStatusTransition) BookkeepingTask
 		errMessage  string
 		errCount    int
-		task        string
+		expectedStatus model.OperationState
 	}{
 		{name: "Mark two operations as orphan in multiple parallel threads", markOpsDone: false, customFunc: func(transition *ClusterStatusTransition) BookkeepingTask {
 			return markOrphanOperation{
 				transition: transition,
 				logger:     logger.NewLogger(true),
 			}
-		}, errMessage: "Bookkeeper failed to update status of orphan operation", errCount: 48, task: "markOrphanOperation"},
+		}, errMessage: "Bookkeeper failed to update status of orphan operation", errCount: 48,  expectedStatus: model.OperationStateOrphan},
 		{name: "Finish two operations in multiple parallel threads", markOpsDone: true, customFunc: func(transition *ClusterStatusTransition) BookkeepingTask {
 			return finishOperation{
 				transition: transition,
 				logger:     logger.NewLogger(true),
 			}
-		}, errMessage: "Bookkeeper failed to update cluster", errCount: 24, task: "finishOperation"},
+		}, errMessage: "Bookkeeper failed to update cluster", errCount: 24,  expectedStatus: model.OperationStateDone},
 	}
 
 	for _, tc := range tests {
@@ -72,25 +157,20 @@ func TestBookkeepingtaskParallel(t *testing.T) {
 
 			//setup bookkeeper task
 			transition := newClusterStatusTransition(dbConn, inventory, reconRepo, logger.NewLogger(true))
-			//trigger bk task
-			bookkeeperOperation := tc.customFunc(transition)
 
 			//initialize bookkeeper
 			bk := newBookkeeper(
 				reconRepo,
 				&BookkeeperConfig{
 					OperationsWatchInterval: 100 * time.Millisecond,
-					OrphanOperationTimeout:  5 * time.Second,
+					OrphanOperationTimeout:  1 * time.Microsecond,
 					MaxRetries:              150,
 				},
 				logger.NewLogger(true),
 			)
 
 			//setup reconciliation result
-			recons, err := reconRepo.GetReconciliations(nil)
-			require.NoError(t, err)
-			reconResult, err := bk.newReconciliationResult(recons[0])
-			require.NoError(t, err)
+			reconResult := getReconResult(t, reconRepo, bk)
 
 			//call Apply in parallel threads
 			errChannel := make(chan error, 100)
@@ -104,11 +184,26 @@ func TestBookkeepingtaskParallel(t *testing.T) {
 					for _, e := range err {
 						errChannel <- e
 					}
-				}(errChannel, bookkeeperOperation)
+				}(errChannel, tc.customFunc(transition))
 			}
 			wg.Wait()
 
+			//check failed amount bookkeepingtasks
 			require.Equal(t, tc.errCount, len(errChannel))
+			//check correct status
+			reconResult = getReconResult(t, reconRepo, bk)
+			operations := reconResult.GetOperations()
+			for _, o := range operations {
+				require.Equal(t,tc.expectedStatus, o.State)
+			}
 		})
 	}
+}
+
+func getReconResult(t *testing.T, reconRepo reconciliation.Repository, bk *bookkeeper) *ReconciliationResult {
+	recons, err := reconRepo.GetReconciliations(nil)
+	require.NoError(t, err)
+	reconResult, err := bk.newReconciliationResult(recons[0])
+	require.NoError(t, err)
+	return reconResult
 }
