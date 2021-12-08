8c1a7a4b31f06de7346d0608800030676b482a64
franpog859 <franciszek.pog@gmail.com>
2021-12-08 14:49:45 +0100

    Run every possible lint script

--- pkg/reconciler/instances/connectivityproxy/command.go
+++ pkg/reconciler/instances/connectivityproxy/command.go
@@ -38,9 +38,7 @@ type CommandActions struct {
 
 func (a *CommandActions) InstallOnReleaseChange(context *service.ActionContext, app *appsv1.StatefulSet) error {
 	if app == nil || (app != nil && app.GetLabels() == nil) {
-		return a.installOnCondition(context, func(s string) (bool, error) {
-			return true, nil
-		})
+		return a.installOnCondition(context, func(_ string) (bool, error) { return true, nil })
 	}
 
 	appName := app.Name
