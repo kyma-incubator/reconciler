86c5a7131665fd83b391715f1601d8a37550b8fa
Ali Khlifi <46890720+khlifi411@users.noreply.github.com>
2022-01-12 16:38:43 +0100

    fix syntax error in drop commands (#663)

--- configs/db/postgres/000008_add_operation_retry_id_and_retries.down.sql
+++ configs/db/postgres/000008_add_operation_retry_id_and_retries.down.sql
@@ -1,2 +1,2 @@
-ALTER TABLE scheduler_operations DROP COLUMN "retry_id" VARCHAR(255);
-ALTER TABLE scheduler_operations DROP COLUMN "retries" int;
+ALTER TABLE scheduler_operations DROP COLUMN "retry_id";
+ALTER TABLE scheduler_operations DROP COLUMN "retries";
