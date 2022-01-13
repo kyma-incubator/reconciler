78b56d887764793a9f857861678e7c3dbb3b327d
Xin Ruan <xin.ruan@sap.com>
2022-01-13 15:00:02 +0100

    remove wrong commited code

--- pkg/reconciler/service/labelsinterceptor.go
+++ pkg/reconciler/service/labelsinterceptor.go
@@ -15,25 +15,6 @@ type LabelsInterceptor struct {
 	Version string
 }
 
-//func (l *LabelsInterceptor) Intercept2(resources kube.ResourceList, _ string) error {
-//	for _, resource := range resources {
-//		resource.Get().
-//	}
-//
-//	interceptorFunc := func(u *unstructured.Unstructured) error {
-//		labels := u.GetLabels()
-//		if labels == nil {
-//			labels = make(map[string]string)
-//		}
-//		labels[ManagedByLabel] = LabelReconcilerValue
-//		labels[KymaVersionLabel] = l.Version
-//		u.SetLabels(labels)
-//		return nil
-//	}
-//
-//	return resources.Visit(interceptorFunc)
-//}
-
 func (l *LabelsInterceptor) Intercept(resources *kubernetes.ResourceCacheList, _ string) error {
 	interceptorFunc := func(u *unstructured.Unstructured) error {
 		labels := u.GetLabels()
