4f99bf0c26807ae5cd268c564f862b12d3b3f35c
Xin Ruan <xin.ruan@sap.com>
2022-01-18 15:44:39 +0100

    skip test

--- pkg/reconciler/kubernetes/adapter_test.go
+++ pkg/reconciler/kubernetes/adapter_test.go
@@ -12,6 +12,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"path/filepath"
 	"testing"
+	"time"
 )
 
 var expectedResourcesWithoutNs = []*Resource{
@@ -89,21 +90,21 @@ func TestCustomerResources(t *testing.T) {
 		require.NoError(t, err)
 	})
 
-	/*TODO: skip until we have better solution to watch CRD deployment progress */
-	//t.Run("Should not get error when deploy CR after CRD", func(t *testing.T) {
-	//	crdManifest := readManifest(t, "unittest-crd.yaml")
-	//
-	//	_, err := kubeClient.Deploy(context.TODO(), crdManifest, "unittest-cr")
-	//	require.NoError(t, err)
-	//
-	//	crManifest := readManifest(t, "unittest-cr.yaml")
-	//
-	//	//have to wait a bit to make sure CRD is fully deployed
-	//	time.Sleep(20 * time.Second)
-	//	_, err = kubeClient.Deploy(context.TODO(), crManifest, "unittest-cr")
-	//
-	//	require.NoError(t, err)
-	//})
+	t.Run("Should not get error when deploy CR after CRD", func(t *testing.T) {
+		t.Skip("skip until we have better solution to watch CRD deployment progress")
+		crdManifest := readManifest(t, "unittest-crd.yaml")
+
+		_, err := kubeClient.Deploy(context.TODO(), crdManifest, "unittest-cr")
+		require.NoError(t, err)
+
+		crManifest := readManifest(t, "unittest-cr.yaml")
+
+		//have to wait a bit to make sure CRD is fully deployed
+		time.Sleep(20 * time.Second)
+		_, err = kubeClient.Deploy(context.TODO(), crManifest, "unittest-cr")
+
+		require.NoError(t, err)
+	})
 }
 
 func deleteTestResources(t *testing.T, client Client) {
