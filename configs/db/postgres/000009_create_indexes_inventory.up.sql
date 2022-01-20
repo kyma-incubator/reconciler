0ae010a7d9612126801563ddc9cd15f6904b6b6d
Jeremy Harisch <48282931+jeremyharisch@users.noreply.github.com>
2022-01-20 14:08:06 +0100

    Create indexes in cluster config statuses (#699)
    * CReate indexes in cluster config statuses
    
    * Typo in file names
    
    * renamed, and more indexes
    
    * Drop indexes

---
+++ configs/db/postgres/000009_create_indexes_inventory.up.sql
@@ -0,0 +1,5 @@
+CREATE INDEX inventory_cluster_config_statuses__idx_deleted_id_status_created ON "inventory_cluster_config_statuses" ("deleted","id","status","created");
+CREATE INDEX inventory_cluster_config_statuses__idx_cluster_version_id ON "inventory_cluster_config_statuses" ("cluster_version","id");
+CREATE INDEX inventory_cluster_configs__idx_deleted_kyma_version_cluster_version_created ON "inventory_cluster_configs" ("deleted","kyma_version","cluster_version","created");
+CREATE INDEX inventory_cluster_configs__idx_cluster_version_version ON "inventory_cluster_configs" ("cluster_version","version");
+CREATE INDEX inventory_cluster__idx_deleted_runtime_id ON "inventory_clusters" ("deleted", "runtime_id");
