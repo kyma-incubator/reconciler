fe3c194e3e4df6403c8aa0601d0cd1e123ecef71
Jeremy Harisch <48282931+jeremyharisch@users.noreply.github.com>
2022-01-31 13:39:59 +0100

    Renamed migration scripts (#735)
    * Renamed migration scripts
    
    * Fix workerpool migration

--- configs/db/postgres/000010_worker_pool_occupancy.up.sql
+++ configs/db/postgres/000010_worker_pool_occupancy.up.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS worker_pool_occupancy (
     "running_workers" int NOT NULL,
     "worker_pool_capacity" int NOT NULL,
     "created" TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc'),
-    CONSTRAINT worker_pool_occupancy_pk PRIMARY KEY ("pool_id")
+    CONSTRAINT worker_pool_occupancy_pk PRIMARY KEY ("worker_pool_id")
     );
