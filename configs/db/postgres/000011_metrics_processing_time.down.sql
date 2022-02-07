ea3011aa0bfcd5930c983195a5968665a1d79302
Jeremy Harisch <jeremy.harisch@sap.com>
2022-02-07 15:26:57 +0100

    Added picked_upcolumn

---
+++ configs/db/postgres/000011_metrics_processing_time.down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE scheduler_operations DROP COLUMN "picked_up";
---
+++ configs/db/postgres/000011_metrics_processing_time.up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE scheduler_operations ADD COLUMN "picked_up" TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc');
