9269a8bc792cc9e536e4cbfbef725697ea0557dd
Jeremy Harisch <jeremy.harisch@sap.com>
2022-02-21 17:53:00 +0100

    DB quick fix

--- configs/db/postgres/000012_update_metrics_processing_duration.up.sql
+++ configs/db/postgres/000012_update_metrics_processing_duration.up.sql
@@ -1,1 +1,2 @@
-UPDATE scheduler_operations SET processing_duration=0 WHERE processing_duration is NULL 
+ALTER TABLE scheduler_operations ALTER COLUMN processing_duration SET DEFAULT 0;
+UPDATE scheduler_operations SET processing_duration=0 WHERE processing_duration is NULL;
