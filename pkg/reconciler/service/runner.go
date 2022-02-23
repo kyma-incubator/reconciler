b5fb0633ac665c1062232a58fd456d270b4f6e55
Stanislav Khalash <stanislav.khalash@sap.com>
2022-02-23 18:12:04 +0100

    WIP: temporarily remove error handling

--- pkg/reconciler/service/runner.go
+++ pkg/reconciler/service/runner.go
@@ -38,18 +38,10 @@ func (r *runner) Run(ctx context.Context, task *reconciler.Task, callback callba
 		} else {
 			manifest, err = r.install.renderManifest(chartProvider, task)
 		}
-		message := ""
-		var status reconciler.Status
-		if err != nil {
-			status = reconciler.StatusFailed
-			message = err.Error()
-		} else {
-			status = reconciler.StatusSuccess
-		}
+
 		callback.Callback(&reconciler.CallbackMessage{
 			Manifest: manifest,
-			Error:    message,
-			Status:   status,
+			Status:   reconciler.StatusSuccess,
 		})
 		return err
 	}
