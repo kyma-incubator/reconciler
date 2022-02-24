5d29898fef6801b4997101cefedd81c6e7efb024
VladislavPaskar <vladislav.paskar@sap.com>
2022-02-24 21:25:27 +0100

    fix linting issues, refactor the fetchJetstreamFlag function

--- pkg/reconciler/instances/eventing/preaction/handledisablejetstream.go
+++ pkg/reconciler/instances/eventing/preaction/handledisablejetstream.go
@@ -48,9 +48,10 @@ func (r *handleEnablingJetstream) Execute(context *service.ActionContext, logger
 
 	// decorate logger
 	logger = logger.With(log.KeyStep, handleDisableJetstream)
-	jetstreamFlag, err := fetchJetstreamFlag(context, logger)
-	if err != nil {
-		return err
+	jetstreamFlag, errorString := fetchJetstreamFlag(context, logger)
+	if errorString != "" {
+		logger.With(log.KeyReason, errorString).Info("Step skipped")
+		return nil
 	}
 
 	logger.Infof("Jetstream is flag is set to %v", jetstreamFlag)
@@ -78,34 +79,41 @@ func (r *handleEnablingJetstream) Execute(context *service.ActionContext, logger
 }
 
 // fetchJetstreamFlag gets the value of ENABLE_JETSTREAM_BACKEND environment var from the eventing controller deployment.
-func fetchJetstreamFlag(context *service.ActionContext, logger *zap.SugaredLogger) (bool, error) {
+func fetchJetstreamFlag(context *service.ActionContext, logger *zap.SugaredLogger) (bool, string) {
 	comp := GetResourcesFromVersion(chartsVersion, chartsName)
 
 	// get the values from eventing/values.yaml
 	configurationValues, err := context.ChartProvider.Configuration(comp)
 	if err != nil {
-		return false, err
+		return false, err.Error()
 	}
 
 	// the enableJetstreamFlag is stored in .Values.global.features.enableJetstream
 	global := configurationValues["global"].(interface{})
-	globalsJson, err := json.Marshal(global)
+	globalsJSON, err2 := json.Marshal(global)
+	if err2 != nil {
+		return false, err2.Error()
+	}
 
 	// the regex should compile and find the necessary flag
 	re := regexp.MustCompile(enableJetstreamRegExp)
-	if !re.MatchString(string(globalsJson)) {
-		logger.With(log.KeyReason, "Cannot find the jetstream Flag in the eventing helm values").Info("Step skipped")
-		return false, nil
+	if !re.MatchString(string(globalsJSON)) {
+		return false, "Cannot find the jetstream Flag in the eventing helm values"
 	}
 
-	matches := re.FindStringSubmatch(string(globalsJson))
+	matches := re.FindStringSubmatch(string(globalsJSON))
+	// first element of the array should be the whole regexp match
+	// the second - the group match
 	if len(matches) != 2 {
 		logger.With(log.KeyReason, "Cannot find the jetstream Flag in the eventing helm values").Info("Step skipped")
-		return false, nil
+		return false, "Cannot find the jetstream Flag in the eventing helm values"
 	}
 
 	jetstreamFlag, err := strconv.ParseBool(matches[1])
-	return jetstreamFlag, err
+	if err != nil {
+		return false, err.Error()
+	}
+	return jetstreamFlag, ""
 }
 
 // getDeployment returns a Kubernetes deployment given its name.
@@ -120,7 +128,7 @@ func getStatefulSetUsingClientSet(context *service.ActionContext, clientset kube
 	return nil, err
 }
 
-// checkJetstreamData checks if the nats-statefulSet contains the jetstream-related data
+// jetstreamDataExists checks if the nats-statefulSet contains the jetstream-related data.
 func jetstreamDataExists(statefulSet *v1.StatefulSet) bool {
 	for _, container := range statefulSet.Spec.Template.Spec.Containers {
 		if !strings.EqualFold(container.Name, natsContainerName) {
