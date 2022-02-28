384b964af4670642ee5a49816985686ea743c289
Jeremy Harisch <jeremy.harisch@sap.com>
2022-02-28 12:26:32 +0100

    Unit test

--- pkg/features/features.go
+++ pkg/features/features.go
@@ -1,21 +1,24 @@
 package features
 
-import "os"
+import (
+	"os"
+	"strings"
+)
 
 const processingDurationMetricEnvVar = "PROCESSING_DURATION_METRICS_ENABLED"
 const workerpoolOccupancyTrackingEnvVar = "WORKERPOOL_OCCUPANCY_TRACKING_ENABLED"
 
 func ProcessingDurationMetricsEnabled() bool {
-	enabled := os.Getenv(processingDurationMetricEnvVar)
-	if enabled == "true" || enabled == "1" {
-		return true
-	}
-	return false
+	return checkEnvVar(processingDurationMetricEnvVar)
 }
 
 func WorkerpoolOccupancyTrackingEnabled() bool {
-	enabled := os.Getenv(workerpoolOccupancyTrackingEnvVar)
-	if enabled == "true" || enabled == "1" {
+	return checkEnvVar(workerpoolOccupancyTrackingEnvVar)
+}
+
+func checkEnvVar(envVar string) bool {
+	enabled := os.Getenv(envVar)
+	if strings.ToLower(enabled) == "true" || enabled == "1" {
 		return true
 	}
 	return false
---
+++ pkg/features/features_test.go
@@ -0,0 +1,45 @@
+package features
+
+import (
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
+
+func TestFeatures(t *testing.T) {
+
+	type testCases struct {
+		description    string
+		funcToTest     func() bool
+		envVar         string
+		envVarValue    string
+		expectedResult bool
+	}
+
+	for _, test := range []testCases{
+		{description: "Workerpool Occupancy set to 1", funcToTest: WorkerpoolOccupancyTrackingEnabled, envVar: workerpoolOccupancyTrackingEnvVar, envVarValue: "1", expectedResult: true},
+		{description: "Workerpool Occupancy set to lowercase true", funcToTest: WorkerpoolOccupancyTrackingEnabled, envVar: workerpoolOccupancyTrackingEnvVar, envVarValue: "true", expectedResult: true},
+		{description: "Workerpool Occupancy set to TrUe; not lowercase", funcToTest: WorkerpoolOccupancyTrackingEnabled, envVar: workerpoolOccupancyTrackingEnvVar, envVarValue: "TrUe", expectedResult: true},
+		{description: "Workerpool Occupancy set to 0", funcToTest: WorkerpoolOccupancyTrackingEnabled, envVar: workerpoolOccupancyTrackingEnvVar, envVarValue: "0", expectedResult: false},
+		{description: "Workerpool Occupancy not set", funcToTest: WorkerpoolOccupancyTrackingEnabled, envVar: workerpoolOccupancyTrackingEnvVar, envVarValue: "", expectedResult: false},
+		{description: "Processing Duration Metric set to 1", funcToTest: ProcessingDurationMetricsEnabled, envVar: processingDurationMetricEnvVar, envVarValue: "1", expectedResult: true},
+		{description: "Processing Duration Metric set to lowercase true", funcToTest: ProcessingDurationMetricsEnabled, envVar: processingDurationMetricEnvVar, envVarValue: "true", expectedResult: true},
+		{description: "Processing Duration Metric set to TrUe; not lowercase", funcToTest: ProcessingDurationMetricsEnabled, envVar: processingDurationMetricEnvVar, envVarValue: "TrUe", expectedResult: true},
+		{description: "Processing Duration Metric set to 0", funcToTest: ProcessingDurationMetricsEnabled, envVar: processingDurationMetricEnvVar, envVarValue: "0", expectedResult: false},
+		{description: "Processing Duration Metric not set", funcToTest: ProcessingDurationMetricsEnabled, envVar: processingDurationMetricEnvVar, envVarValue: "", expectedResult: false},
+	} {
+		t.Run(test.description, func(t *testing.T) {
+			if test.envVarValue != "" {
+				err := os.Setenv(test.envVar, test.envVarValue)
+				require.NoError(t, err)
+			}
+
+			actualResult := test.funcToTest()
+			require.Equal(t, test.expectedResult, actualResult)
+
+			err := os.Unsetenv(test.envVar)
+			require.NoError(t, err)
+		})
+	}
+
+}
