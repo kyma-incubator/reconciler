ac0de0f8503a04ad900ad12a5867a34590550e71
Przemyslaw Golicz <przemyslaw.golicz@sap.com>
2022-03-01 09:57:01 +0100

    add client creafion function

--- pkg/reconciler/instances/provisioning/action.go
+++ pkg/reconciler/instances/provisioning/action.go
@@ -1,6 +1,16 @@
 package provisioning
 
-import "github.com/kyma-incubator/reconciler/pkg/reconciler/service"
+import (
+	"fmt"
+	file "github.com/kyma-incubator/reconciler/pkg/files"
+	"github.com/kyma-incubator/reconciler/pkg/reconciler/instances/provisioning/gardener"
+	"github.com/kyma-incubator/reconciler/pkg/reconciler/service"
+	"io/ioutil"
+	restclient "k8s.io/client-go/rest"
+	"os"
+)
+
+const envVarGardenerKubeconfig = "GARDENER_KUBECONFIG_PATH"
 
 type ProvisioningAction struct {
 	name       string
@@ -15,4 +25,23 @@ func (a *ProvisioningAction) Run(context *service.ActionContext) error {
 	context.Logger.Infof("Action '%s' executed (passed version was '%s')", a.name, context.Task.Version)
 
 	return nil
+}
+
+func newGardenerClusterConfig() (*restclient.Config, error) {
+
+	if !file.Exists(os.Getenv(envVarGardenerKubeconfig)) {
+		return nil, fmt.Errorf("kubeconfig file does not exist in path %s", envVarGardenerKubeconfig)
+	}
+
+	rawKubeconfig, err := ioutil.ReadFile(envVarGardenerKubeconfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read Gardener Kubeconfig from path %s: %s", envVarGardenerKubeconfig, err.Error())
+	}
+
+	gardenerClusterConfig, err := gardener.Config(rawKubeconfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Gardener cluster config: %s", err.Error())
+	}
+
+	return gardenerClusterConfig, nil
 }
