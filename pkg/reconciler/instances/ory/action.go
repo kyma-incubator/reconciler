45791121527de38a09167a81b0b44d98b7f34fb5
dariusztutaj <dariusz.tutaj@sap.com>
2022-03-07 08:06:34 +0100

    investigate pre-main-control-plane-reconciler-upgrade-kyma2-latest-to-main-gardener job

--- pkg/reconciler/instances/ory/action.go
+++ pkg/reconciler/instances/ory/action.go
@@ -66,7 +66,7 @@ func (a *postReconcileAction) Run(context *service.ActionContext) error {
 
 	if isInMemoryMode(cfg) {
 		logger.Debug("Detected in hydra in memory mode, triggering synchronization")
-		logger.Debug("=============== ORY hydra-maester pods BEGIN ===============")
+		logger.Debug("=============== ORY pods BEGIN ===============")
 		clientset, _ := kubeclient.Clientset()
 		podList, err := clientset.CoreV1().Pods(oryNamespace).List(context.Context, metav1.ListOptions{
 			LabelSelector: "app.kubernetes.io/instance=ory"})
@@ -76,7 +76,7 @@ func (a *postReconcileAction) Run(context *service.ActionContext) error {
 		for _, pod := range podList.Items {
 			logger.Debugf("%s : %s", pod.Name, pod.CreationTimestamp)
 		}
-		logger.Debug("=============== ORY hydra-maester pods END ===============")
+		logger.Debug("=============== ORY pods END ===============")
 		err = a.hydraSyncer.TriggerSynchronization(context.Context, kubeclient, logger, oryNamespace, rolloutHydra)
 		if err != nil {
 			return errors.Wrap(err, "failed to trigger hydra sychronization")
