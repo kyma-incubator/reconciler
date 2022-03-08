f64063b49027db8bf796431e01db2be71e217873
d067928 <jakob.moeller@sap.com>
2022-03-08 16:55:20 +0100

    feat: create views for inventory calls
    add created fields in views to allow for full backwards-compat

--- configs/db/postgres/000013_active_cluster_views.up.sql
+++ configs/db/postgres/000013_active_cluster_views.up.sql
@@ -16,10 +16,11 @@ SELECT DISTINCT ON (config_id) -- if this shows an error for you, its not a prob
                                id              AS status_id,
                                config_version  AS config_id,
                                cluster_version AS cluster_id,
-                               status
+                               status,
+                               created         AS status_created_at
 FROM inventory_cluster_config_statuses
 WHERE deleted = false
-GROUP BY status_id, cluster_id, config_id, status
+GROUP BY status_id, cluster_id, config_id, status, status_created_at
 ORDER BY config_id, status_id DESC;
 
 CREATE OR REPLACE VIEW v_active_inventory_cluster_latest_status
@@ -28,7 +29,8 @@ SELECT vaicc.config_id,
        vaicc.cluster_id,
        vaicc.runtime_id,
        vaiccsh.status_id,
-       vaiccsh.status
+       vaiccsh.status,
+       vaiccsh.status_created_at
 FROM v_active_inventory_cluster_configs vaicc
          JOIN v_active_inventory_cluster_config_status_history vaiccsh
               ON vaicc.config_id = vaiccsh.config_id;
@@ -40,14 +42,17 @@ SELECT latest_status.config_id,
        latest_status.runtime_id,
        latest_status.status_id,
        latest_status.status,
+       latest_status.status_created_at,
        icc.kyma_version,
        icc.kyma_profile,
        icc.components,
        icc.administrators,
+       icc.created as config_created_at,
        ic.runtime,
        ic.metadata,
        ic.kubeconfig,
-       ic.contract
+       ic.contract,
+       ic.created  as cluster_created_at
 FROM v_active_inventory_cluster_latest_status latest_status
          JOIN inventory_cluster_configs icc
               ON
