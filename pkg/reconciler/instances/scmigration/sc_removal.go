c025d43d783da295295ce9f568516121757cea5c
Jan Wozniak <wozniak.jan@gmail.com>
2022-03-24 16:02:47 +0100

    Safe cleanup of service catalog instances (#955)
    * Revert "Service catalog migration keep old instances and bindings"
    
    This reverts commit 122087b32a7ee811f2046abd3064761d3ba3d799.
    
    * Safe delete of Service Catalog instances

--- pkg/reconciler/instances/scmigration/sc_removal.go
+++ pkg/reconciler/instances/scmigration/sc_removal.go
@@ -156,6 +156,16 @@ func (c *scremoval) prepareForRemoval(ac *service.ActionContext) error {
 	gvkList := []schema.GroupVersionKind{
 		{
 			Group:   "servicecatalog.k8s.io",
+			Kind:    "ServiceBindingList",
+			Version: "v1beta1",
+		},
+		{
+			Group:   "servicecatalog.k8s.io",
+			Kind:    "ServiceInstanceList",
+			Version: "v1beta1",
+		},
+		{
+			Group:   "servicecatalog.k8s.io",
 			Kind:    "ServiceBrokerList",
 			Version: "v1beta1",
 		},
@@ -258,6 +268,16 @@ func (c *scremoval) removeResources(ac *service.ActionContext) error {
 			Kind:    "UsageKind",
 			Group:   "servicecatalog.kyma-project.io",
 			Version: "v1alpha1",
+		},
+		{
+			Kind:    "ServiceBinding",
+			Group:   "servicecatalog.k8s.io",
+			Version: "v1beta1",
+		},
+		{
+			Kind:    "ServiceInstance",
+			Group:   "servicecatalog.k8s.io",
+			Version: "v1beta1",
 		},
 		{
 			Kind:    "ServiceBroker",
