352926347374157e0329ed0cd8a92bfe9512ae3c
d067928 <jakob.moeller@sap.com>
2022-04-06 15:52:48 +0200

    !feat: remove audit log functionality until better concept is in place

--- cmd/mothership/mothership/start/auditlog.go
+++ cmd/mothership/mothership/start/auditlog.go
@@ -1,22 +1,10 @@
 package cmd
 
 import (
-	"bytes"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"time"
-
-	"github.com/kyma-incubator/reconciler/pkg/keb"
-	"github.com/kyma-incubator/reconciler/pkg/server"
-
-	"github.com/google/uuid"
-	"github.com/pkg/errors"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
+	"net/http"
 )
 
 const (
@@ -68,130 +56,11 @@ func newAuditLoggerMiddelware(l *zap.Logger, o *Options) func(http.Handler) http
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			auditLogRequest(w, r, l, o)
-
 			next.ServeHTTP(w, r)
 		})
 	}
 }
 
-type data struct {
-	ContractVersion int64  `json:"contractVersion"`
-	Method          string `json:"method"`
-	URI             string `json:"uri"`
-	RequestBody     string `json:"requestBody"`
-	User            string `json:"user"`
-	Tenant          string `json:"tenant"`
-	IP              string `json:"ip"`
-}
-
 func auditLogRequest(w http.ResponseWriter, r *http.Request, l *zap.Logger, o *Options) {
-
-	// Any Audit Log relevant entry will be a stateful change in POST/PUT/PATCH, GET can be ignored
-	if r.Method == http.MethodGet {
-		return
-	}
-
-	params := server.NewParams(r)
-	contractV, err := params.Int64(paramContractVersion)
-	if err != nil {
-
-		server.SendHTTPError(w, http.StatusBadRequest, &keb.HTTPErrorResponse{
-			Error: errors.Wrap(err, "Contract version undefined").Error(),
-		})
-		return
-	}
-	logData := data{
-		ContractVersion: contractV,
-		Method:          r.Method,
-		URI:             r.RequestURI,
-		User:            "UNKNOWN_USER",
-		Tenant:          o.AuditLogTenantID,
-		IP:              "",
-	}
-
-	jwtPayload, err := getJWTPayload(r)
-	if err != nil {
-		server.SendHTTPError(w, http.StatusInternalServerError, &keb.HTTPErrorResponse{
-			Error: errors.Wrap(err, fmt.Sprintf("Failed to parse %s header content ", XJWTHeaderName)).Error(),
-		})
-		return
-	}
-
-	user, err := getJWTPayloadSub(jwtPayload)
-	if err != nil {
-		server.SendHTTPError(w, http.StatusInternalServerError, &keb.HTTPErrorResponse{
-			Error: errors.Wrap(err, "failed to Unmarshal JWT payload").Error(),
-		})
-		return
-	}
-
-	if user != "" {
-		logData.User = user
-	}
-
-	// log request body if needed.
-	if r.Method == "POST" || r.Method == "PUT" {
-		reqBody, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			server.SendHTTPError(w, http.StatusInternalServerError, &keb.HTTPErrorResponse{
-				Error: errors.Wrap(err, "Failed to read received JSON payload").Error(),
-			})
-			return
-		}
-		r.Body = ioutil.NopCloser(bytes.NewBuffer(reqBody))
-		logData.RequestBody = string(reqBody)
-	}
-
-	externalHeaderIP := r.Header.Get(ExternalAddressHeaderName)
-	if externalHeaderIP == "" {
-		o.Logger().Debug(fmt.Sprintf("empty %s header", ExternalAddressHeaderName))
-	} else {
-		logData.IP = externalHeaderIP
-	}
-
-	data, err := json.Marshal(logData)
-	if err != nil {
-		server.SendHTTPError(w, http.StatusInternalServerError, &keb.HTTPErrorResponse{
-			Error: errors.Wrap(err, "Failed to marshal auditlog JSON payload").Error(),
-		})
-		return
-	}
-
-	logger := l.With(zap.String("time", time.Now().Format(time.RFC3339))).
-		With(zap.String("uuid", uuid.New().String())).
-		With(zap.String("user", logData.User)).
-		With(zap.String("data", string(data))).
-		With(zap.String("tenant", o.AuditLogTenantID)).
-		With(zap.String("category", "audit.security-events")) // comply with required log backend format
-
-	if logData.IP != "" {
-		logger = logger.With(zap.String("ip", logData.IP))
-	}
-
-	logger.Info("")
-}
-
-func getJWTPayload(r *http.Request) (string, error) {
-	// The jwtHeader here is not a full JWT token. Instead, it's only the
-	// encoded payload part of the token. It's passed by Istio as a header
-	// since Authz/Authn is done by Istio, and we don't receive the original full JWT token.
-	jwtHeader := r.Header.Get(XJWTHeaderName)
-	if len(jwtHeader) == 0 {
-		return "", nil
-	}
-	decodedSeg, err := base64.RawURLEncoding.DecodeString(jwtHeader)
-	return string(decodedSeg), err
-}
-
-type jwtSub struct {
-	Sub string `json:"sub"`
-}
-
-func getJWTPayloadSub(payload string) (string, error) {
-	if payload == "" {
-		return "", nil
-	}
-	s := jwtSub{}
-	err := json.Unmarshal([]byte(payload), &s)
-	return s.Sub, err
+	//TODO Change or remove once Audit Log decision is made
 }
