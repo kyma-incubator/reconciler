c24dcd87438fc9d56fcbfe6e240ddb167a33d8a5
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2022-04-20 10:58:42 +0200

    Delete issue

--- pkg/reconciler/instances/connectivityproxy/command.go
+++ pkg/reconciler/instances/connectivityproxy/command.go
@@ -136,13 +136,13 @@ func (a *CommandActions) Remove(context *service.ActionContext) error {
 	}
 
 	context.Logger.Info("Removing cert secrets")
-	_, err = context.KubeClient.DeleteResource(context.Context, "secret", "cc-certs", "istio-namespace")
+	_, err = context.KubeClient.DeleteResource(context.Context, "Secret", "cc-certs", "istio-namespace")
 	if err != nil {
 		context.Logger.Error("Error during removal of cc-certs in istio-namespace")
 		return errors.Wrap(err, "Error during removal of cc-certs in istio-namespace")
 	}
 
-	_, err = context.KubeClient.DeleteResource(context.Context, "secret", "cc-certs-cacert", "istio-namespace")
+	_, err = context.KubeClient.DeleteResource(context.Context, "Secret", "cc-certs-cacert", "istio-namespace")
 	if err != nil {
 		context.Logger.Info("Error during removal of cc-certs-cacert in istio-namespace")
 		return errors.Wrap(err, "Error during removal of cc-certs-cacert in istio-namespace")
