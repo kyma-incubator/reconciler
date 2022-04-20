d21f696537331f3512c2743b8d2d3c9734f1b795
Aditya Bhatia <aditya.bhatia@sap.com>
2022-04-20 20:40:26 +0200

    separate migrate up and down scripts + improvements

--- configs/db/postgres/000017_streamline_fks_indices.down.sql
+++ configs/db/postgres/000017_streamline_fks_indices.down.sql
@@ -1,23 +1,29 @@
 ---- cluster status table
 -- drop fk dependency
 ALTER TABLE scheduler_reconciliations
-    DROP CONSTRAINT scheduler_reconciliations_cluster_config_status_fkey;
+DROP CONSTRAINT scheduler_reconciliations_cluster_config_status_fkey;
 
--- drop "id" as key
-ALTER SEQUENCE inventory_cluster_config_statuses_id_seq AS INTEGER ;
-
+-- drop "id" as primary key
 ALTER TABLE inventory_cluster_config_statuses
-    DROP CONSTRAINT inventory_cluster_config_statuses_id_key;
+DROP CONSTRAINT inventory_cluster_config_statuses_pkey;
+
+-- add "id" as key
+ALTER TABLE inventory_cluster_config_statuses
+    ADD UNIQUE (id);
 
 ---- scheduler_reconciliations table
--- drop cluster config fk
-ALTER TABLE scheduler_reconciliations
-    DROP CONSTRAINT scheduler_reconciliations_cluster_config_fkey;
+-- drop index for cluster status fk constraint
+DROP INDEX if EXISTS scheduler_reconciliations_cluster_config_status_idx;
 
--- drop fk to cluster status
+-- add fk to cluster status
 ALTER TABLE scheduler_reconciliations
-    DROP CONSTRAINT scheduler_reconciliations_cluster_config_status_fkey;
+    ADD FOREIGN KEY (cluster_config_status) references inventory_cluster_config_statuses (id);
+
+-- add cluster config fk
+ALTER TABLE scheduler_reconciliations
+    ADD FOREIGN KEY (cluster_config) references inventory_cluster_configs(version);
 
 ---- scheduler_operations table
+-- add cluster config fk
 ALTER TABLE scheduler_operations
-    DROP CONSTRAINT scheduler_operations_cluster_config_fkey;
+    ADD FOREIGN KEY (cluster_config) references inventory_cluster_configs(version);
