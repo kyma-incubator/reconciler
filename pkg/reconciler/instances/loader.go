23a096e0ce7ac08563b695648d2c5094ad125474
Kwiatosz, Krzysztof <krzysztof.kwiatosz@sap.com>
2022-05-05 15:57:55 +0200

    Drop rafter reconciler

--- pkg/reconciler/instances/loader.go
+++ pkg/reconciler/instances/loader.go
@@ -19,8 +19,6 @@ import (
 	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/istio"
 	//import required to register component reconciler 'ory' in reconciler registry
 	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/ory"
-	//import required to register component reconciler 'rafter' in reconciler registry
-	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/rafter"
 	//import required to register component reconciler 'rma' in reconciler registry
 	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/rma"
 	//import required to register component reconciler 'scmigration' in reconciler registry
