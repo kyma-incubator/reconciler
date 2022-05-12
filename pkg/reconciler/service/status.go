a234b0e4231f36f441184b8d004fcd56d5c34aca
Jeremy Harisch <jeremy.harisch@sap.com>
2022-05-12 10:39:32 +0200

    Fix not creating bug

--- pkg/reconciler/service/status.go
+++ pkg/reconciler/service/status.go
@@ -6,6 +6,7 @@ import (
 	"github.com/kyma-incubator/reconciler/pkg/reconciler"
 	"go.uber.org/zap"
 	"k8s.io/apimachinery/pkg/api/errors"
+	"strings"
 	"time"
 
 	k8s "github.com/kyma-incubator/reconciler/pkg/reconciler/kubernetes"
@@ -14,26 +15,24 @@ import (
 )
 
 func createOrUpdateStatusCm(ctx context.Context, task *reconciler.Task, status reconciler.Status, kubeclient k8s.Client, logger *zap.SugaredLogger) error {
-
-	configMapName := fmt.Sprintf("%s-status", task.Component)
+	configMapName := fmt.Sprintf("%s-status", strings.ToLower(task.Component))
 	clientset, err := kubeclient.Clientset()
 	if err != nil {
+		logger.Errorf("Error getting clientset: %s", err)
 		return err
 	}
-
 	_, err = clientset.CoreV1().Namespaces().Get(ctx, task.Namespace, metav1.GetOptions{})
 	if errors.IsNotFound(err) {
 		logger.Debugf("Namespace %s not found, status ConfigMap will only be created if namespace exists", task.Namespace)
 		return nil
 	}
-
 	// Get ConfigMap
 	configMap, err := clientset.CoreV1().ConfigMaps(task.Namespace).Get(ctx, configMapName, metav1.GetOptions{})
 	if errors.IsNotFound(err) {
 		// ConfigMap does not exist, create new one
 		logger.Debugf("ConfigMap '%s' not found, creating new one", configMapName)
 		lastReconciliation := ""
-		if status == reconciler.StatusSuccess {
+		if status != reconciler.StatusRunning && status != reconciler.StatusNotstarted {
 			lastReconciliation = time.Now().String()
 		}
 		configFile := map[string]string{
@@ -50,13 +49,17 @@ func createOrUpdateStatusCm(ctx context.Context, task *reconciler.Task, status r
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      configMapName,
 				Namespace: task.Namespace,
-				Labels:    map[string]string{}, // TODO fill in correct labels
+				Labels: map[string]string{
+					"reconciler.kyma-project.io/managed-by":             "reconciler",
+					"reconciler.kyma-project.io/origin-version":         task.Version,
+					"reconciler.kyma-project.io/reconciliation-summary": "true",
+				},
 			},
 			Data: configFile,
 		}
 		_, err := clientset.CoreV1().ConfigMaps(task.Namespace).Create(ctx, &cm, metav1.CreateOptions{})
 		if err != nil {
-			logger.Errorf("Error after creating ConfigMap '%s': %s", configMapName, err)
+			logger.Debugf("Error after creating ConfigMap '%s': %s", configMapName, err)
 			return err
 		}
 
@@ -69,15 +72,15 @@ func createOrUpdateStatusCm(ctx context.Context, task *reconciler.Task, status r
 		logger.Debugf("ConfigMap '%s' found, updating status", configMapName)
 		configMap.Data["version"] = task.Version
 		configMap.Data["status"] = string(status)
-		if status == reconciler.StatusSuccess {
+		if status != reconciler.StatusRunning && status != reconciler.StatusNotstarted {
 			configMap.Data["last-reconciliation"] = time.Now().String()
 		}
 		_, err := clientset.CoreV1().ConfigMaps(task.Namespace).Update(ctx, configMap, metav1.UpdateOptions{})
 		if err != nil {
-			logger.Errorf("Error updating ConfigMap '%s': %s", configMapName, err)
+			logger.Debugf("Error updating ConfigMap '%s': %s", configMapName, err)
 			return err
 		}
+		logger.Debugf("ConfigMap '%s' successfully updated", configMapName)
 	}
-
 	return nil
 }
