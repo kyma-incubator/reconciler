b2a79a3d008e07b2af69c3207db051244691e2d2
Karol Szwaj <karol.szwaj@gmail.com>
2022-06-06 12:13:16 +0200

    Remove istioctl 1.11.4 from Dockerfile (#1088)

--- Dockerfile.cr
+++ Dockerfile.cr
@@ -1,5 +1,4 @@
 # Istioctl source images
-FROM eu.gcr.io/kyma-project/external/istio/istioctl:1.11.4 AS istio-1_11_4
 FROM eu.gcr.io/kyma-project/external/istio/istioctl:1.12.3 AS istio-1_12_3
 FROM eu.gcr.io/kyma-project/external/istio/istioctl:1.13.2 AS istio-1_13_2
 
@@ -38,11 +37,10 @@ COPY --from=build /bin/reconciler /bin/reconciler
 COPY --from=build /configs/ /configs/
 
 # Add istioctl tools
-COPY --from=istio-1_11_4 /usr/local/bin/istioctl /bin/istioctl-1.11.4
 COPY --from=istio-1_12_3 /usr/local/bin/istioctl /bin/istioctl-1.12.3
 COPY --from=istio-1_13_2 /usr/local/bin/istioctl /bin/istioctl-1.13.2
 # For multiple istioctl binaries, provide their paths separated with a semicolon (;) like in the Linux PATH variable.
-ENV ISTIOCTL_PATH=/bin/istioctl-1.12.3;/bin/istioctl-1.11.4;/bin/istioctl-1.13.2
+ENV ISTIOCTL_PATH=/bin/istioctl-1.12.3;/bin/istioctl-1.13.2
 
 USER appuser:appuser
 
