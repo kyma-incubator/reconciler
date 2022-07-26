e33fd4b96a3595b5840d17c88e222aafea1d56f7
Johannes Veicht <johannes.veicht@sap.com>
2022-07-26 09:06:44 +0200

    Only patch new istio webhook (#1139)

--- pkg/reconciler/instances/istio/istio_test.go
+++ pkg/reconciler/instances/istio/istio_test.go
@@ -287,7 +287,7 @@ func newFakeKubeClient() *k8smocks.Client {
 		},
 	},
 		&v12.MutatingWebhookConfiguration{
-			ObjectMeta: metav1.ObjectMeta{Name: "istio-sidecar-injector"},
+			ObjectMeta: metav1.ObjectMeta{Name: "istio-revision-tag-default"},
 			Webhooks: []v12.MutatingWebhook{
 				{
 					Name: "auto.sidecar-injector.istio.io",
