0be39e163e364ae6cb7dda851fe96b22cd79c4e8
dariusztutaj <dariusz.tutaj@sap.com>
2022-08-10 22:30:14 +0200

    Updating mocks

--- pkg/reconciler/instances/istio/reset/data/mocks/Gatherer.go
+++ pkg/reconciler/instances/istio/reset/data/mocks/Gatherer.go
@@ -41,29 +41,6 @@ func (_m *Gatherer) GetAllPods(kubeClient kubernetes.Interface, retryOpts []retr
 	return r0, r1
 }
 
-// GetPodsInIstioMesh provides a mock function with given fields: kubeClient, retryOpts, sidecarInjectionEnabledbyDefault
-func (_m *Gatherer) GetPodsInIstioMesh(kubeClient kubernetes.Interface, retryOpts []retry.Option, sidecarInjectionEnabledbyDefault bool) (*v1.PodList, error) {
-	ret := _m.Called(kubeClient, retryOpts, sidecarInjectionEnabledbyDefault)
-
-	var r0 *v1.PodList
-	if rf, ok := ret.Get(0).(func(kubernetes.Interface, []retry.Option, bool) *v1.PodList); ok {
-		r0 = rf(kubeClient, retryOpts, sidecarInjectionEnabledbyDefault)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(*v1.PodList)
-		}
-	}
-
-	var r1 error
-	if rf, ok := ret.Get(1).(func(kubernetes.Interface, []retry.Option, bool) error); ok {
-		r1 = rf(kubeClient, retryOpts, sidecarInjectionEnabledbyDefault)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
 // GetPodsWithDifferentImage provides a mock function with given fields: inputPodsList, image
 func (_m *Gatherer) GetPodsWithDifferentImage(inputPodsList v1.PodList, image data.ExpectedImage) v1.PodList {
 	ret := _m.Called(inputPodsList, image)
@@ -78,20 +55,20 @@ func (_m *Gatherer) GetPodsWithDifferentImage(inputPodsList v1.PodList, image da
 	return r0
 }
 
-// GetPodsWithoutSidecar provides a mock function with given fields: kubeClient, retryOpts
-func (_m *Gatherer) GetPodsWithoutSidecar(kubeClient kubernetes.Interface, retryOpts []retry.Option) (v1.PodList, error) {
-	ret := _m.Called(kubeClient, retryOpts)
+// GetPodsWithoutSidecar provides a mock function with given fields: kubeClient, retryOpts, sidecarInjectionEnabledbyDefault
+func (_m *Gatherer) GetPodsWithoutSidecar(kubeClient kubernetes.Interface, retryOpts []retry.Option, sidecarInjectionEnabledbyDefault bool) (v1.PodList, error) {
+	ret := _m.Called(kubeClient, retryOpts, sidecarInjectionEnabledbyDefault)
 
 	var r0 v1.PodList
-	if rf, ok := ret.Get(0).(func(kubernetes.Interface, []retry.Option) v1.PodList); ok {
-		r0 = rf(kubeClient, retryOpts)
+	if rf, ok := ret.Get(0).(func(kubernetes.Interface, []retry.Option, bool) v1.PodList); ok {
+		r0 = rf(kubeClient, retryOpts, sidecarInjectionEnabledbyDefault)
 	} else {
 		r0 = ret.Get(0).(v1.PodList)
 	}
 
 	var r1 error
-	if rf, ok := ret.Get(1).(func(kubernetes.Interface, []retry.Option) error); ok {
-		r1 = rf(kubeClient, retryOpts)
+	if rf, ok := ret.Get(1).(func(kubernetes.Interface, []retry.Option, bool) error); ok {
+		r1 = rf(kubeClient, retryOpts, sidecarInjectionEnabledbyDefault)
 	} else {
 		r1 = ret.Error(1)
 	}
