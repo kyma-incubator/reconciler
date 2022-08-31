dcb7dbd7a3c9b830d83f132e70bf1f62bbc1001e
Bartosz Chwila <bartosz.chwila@sap.com>
2022-08-31 13:04:21 +0200

    Rename error

--- pkg/reconciler/instances/istio/reset/config/errors.go
+++ pkg/reconciler/instances/istio/reset/config/errors.go
@@ -1,5 +1,5 @@
 package config
 
 const (
-	Error_CouldNotLabelWithWarning string = "Could not label pod outside of istio mesh with warning"
+	ErrorCouldNotLabelWithWarning string = "Could not label pod outside of istio mesh with warning"
 )
