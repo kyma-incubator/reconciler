817c84bb093bc59d8fed33398037b7978db61f87
Bartosz Chwila <bartosz.chwila@sap.com>
2022-08-31 13:17:57 +0200

    Add comment

--- pkg/reconciler/instances/istio/reset/config/consts.go
+++ pkg/reconciler/instances/istio/reset/config/consts.go
@@ -3,5 +3,5 @@ package config
 const (
 	KymaWarning         string = "kyma-warning"
 	NotInIstioMeshLabel string = "pod-not-in-istio-mesh"
-	LabelFormat         string = `{"metadata": {"labels": {"%s": "%s"}}}`
+	LabelFormat         string = `{"metadata": {"labels": {"%s": "%s"}}}` // Will use KymaWarning and NotInIstioMeshLabel
 )
