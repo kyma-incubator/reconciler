af6fe612f53f39c89dc1d7d47f9c87ab104a402c
Bartosz Chwila <bartosz.chwila@sap.com>
2022-11-29 11:53:59 +0100

    Update tests and operator image

--- pkg/reconciler/instances/istio/actions/performer_ig_test.go
+++ pkg/reconciler/instances/istio/actions/performer_ig_test.go
@@ -146,6 +146,93 @@ func Test_DefaultIstioPerfomer_InstallIGRestart(t *testing.T) {
 		dep := appsv1.Deployment{}
 		err = ctrlClientDiffConfig.Get(context.TODO(), types.NamespacedName{Namespace: depNamespace, Name: depName}, &dep)
 		require.NoError(t, err)
+		require.NotEmpty(t, dep.Spec.Template.Annotations["reconciler.kyma-project.io/lastRestartDate"])
+	})
+}
+
+func Test_DefaultIstioPerfomer_UpdateIGRestart(t *testing.T) {
+	kubeConfig := "kubeConfig"
+	log := logger.NewLogger(false)
+
+	t.Run("should not restart IG when no config changed", func(t *testing.T) {
+		// given
+		ctrlClientSameConfig := GetIGClient(t, TestConfigMap)
+		numTrustedProxies := 3
+
+		istioCR := &v1alpha1.Istio{ObjectMeta: metav1.ObjectMeta{
+			Name:      "istio-test",
+			Namespace: "namespace",
+		},
+			Spec: v1alpha1.IstioSpec{
+				Config: v1alpha1.Config{
+					NumTrustedProxies: &numTrustedProxies,
+				},
+			},
+		}
+
+		ctrlClientSameConfig.Create(context.TODO(), istioCR)
+
+		cmder := istioctlmocks.Commander{}
+		cmder.On("Upgrade", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("*zap.SugaredLogger")).Return(nil)
+		cmdResolver := TestCommanderResolver{cmder: &cmder}
+
+		proxy := proxymocks.IstioProxyReset{}
+		provider := clientsetmocks.Provider{}
+		provider.On("GetIstioClient", mock.Anything).Return(ctrlClientSameConfig, nil)
+		provider.On("RetrieveFrom", mock.AnythingOfType("string"), mock.AnythingOfType("*zap.SugaredLogger")).Return(fake.NewSimpleClientset(), nil)
+
+		wrapper := NewDefaultIstioPerformer(cmdResolver, &proxy, &provider)
+
+		// when
+		err := wrapper.Update(context.TODO(), kubeConfig, istioManifest, "1.2.3", log)
+
+		// then
+		require.NoError(t, err)
+
+		dep := appsv1.Deployment{}
+		err = ctrlClientSameConfig.Get(context.TODO(), types.NamespacedName{Namespace: depNamespace, Name: depName}, &dep)
+		require.NoError(t, err)
 		require.Empty(t, dep.Spec.Template.Annotations["reconciler.kyma-project.io/lastRestartDate"])
+	})
+
+	t.Run("should restart IG when config changed", func(t *testing.T) {
+		// given
+		ctrlClientDiffConfig := GetIGClient(t, TestConfigMap)
+		numTrustedProxies := 2
+
+		istioCR := &v1alpha1.Istio{ObjectMeta: metav1.ObjectMeta{
+			Name:      "istio-test",
+			Namespace: "namespace",
+		},
+			Spec: v1alpha1.IstioSpec{
+				Config: v1alpha1.Config{
+					NumTrustedProxies: &numTrustedProxies,
+				},
+			},
+		}
+
+		ctrlClientDiffConfig.Create(context.TODO(), istioCR)
+
+		cmder := istioctlmocks.Commander{}
+		cmder.On("Upgrade", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("*zap.SugaredLogger")).Return(nil)
+		cmdResolver := TestCommanderResolver{cmder: &cmder}
+
+		proxy := proxymocks.IstioProxyReset{}
+		provider := clientsetmocks.Provider{}
+		provider.On("GetIstioClient", mock.Anything).Return(ctrlClientDiffConfig, nil)
+		provider.On("RetrieveFrom", mock.AnythingOfType("string"), mock.AnythingOfType("*zap.SugaredLogger")).Return(fake.NewSimpleClientset(), nil)
+
+		wrapper := NewDefaultIstioPerformer(cmdResolver, &proxy, &provider)
+
+		// when
+		err := wrapper.Update(context.TODO(), kubeConfig, istioManifest, "1.2.3", log)
+
+		// then
+		require.NoError(t, err)
+
+		dep := appsv1.Deployment{}
+		err = ctrlClientDiffConfig.Get(context.TODO(), types.NamespacedName{Namespace: depNamespace, Name: depName}, &dep)
+		require.NoError(t, err)
+		require.NotEmpty(t, dep.Spec.Template.Annotations["reconciler.kyma-project.io/lastRestartDate"])
 	})
 }
