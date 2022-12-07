f5c08eb2e8d314e8731c30f46f436ef7ccf6c504
Korbinian Stoemmer <github@k15r.dev>
2022-12-07 14:16:18 +0100

    remove eventing reconciler (#1254)

--- pkg/reconciler/instances/loader.go
+++ pkg/reconciler/instances/loader.go
@@ -3,22 +3,20 @@
 package instances
 
 import (
-	//import required to register component reconciler 'base' in reconciler registry
+	// import required to register component reconciler 'base' in reconciler registry
 	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/base"
-	//import required to register component reconciler 'cleaner' in reconciler registry
+	// import required to register component reconciler 'cleaner' in reconciler registry
 	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/cleaner"
-	//import required to register component reconciler 'clusteressentials' in reconciler registry
+	// import required to register component reconciler 'clusteressentials' in reconciler registry
 	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/clusteressentials"
-	//import required to register component reconciler 'connectivityproxy' in reconciler registry
+	// import required to register component reconciler 'connectivityproxy' in reconciler registry
 	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/connectivityproxy"
-	//import required to register component reconciler 'eventing' in reconciler registry
-	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/eventing"
-	//import required to register component reconciler 'istio' in reconciler registry
+	// import required to register component reconciler 'istio' in reconciler registry
 	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/istio"
-	//import required to register component reconciler 'ory' in reconciler registry
+	// import required to register component reconciler 'ory' in reconciler registry
 	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/ory"
-	//import required to register component reconciler 'rma' in reconciler registry
+	// import required to register component reconciler 'rma' in reconciler registry
 	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/rma"
-	//import required to register component reconciler 'serverless' in reconciler registry
+	// import required to register component reconciler 'serverless' in reconciler registry
 	_ "github.com/kyma-incubator/reconciler/pkg/reconciler/instances/serverless"
 )
