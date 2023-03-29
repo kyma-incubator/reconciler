923d8e55e4d108e048416926d29a552d8936a6aa
Benjamin Lindner <benjamin.lindner@sap.com>
2023-03-29 13:17:35 +0200

    Bump linter version and move excludes to config file

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -8,13 +8,11 @@ set -E         # needs to be set if we want the ERR trap
 readonly CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 readonly ROOT_PATH=$( cd "${CURRENT_DIR}/.." && pwd )
 readonly TMP_DIR=$(mktemp -d)
-readonly GOLANGCI_LINT_VERSION="v1.45.2"
-
+readonly GOLANGCI_LINT_VERSION="v1.52.2"
 
 readonly RED='\033[0;31m'
 readonly GREEN='\033[0;32m'
 readonly NC='\033[0m' # No Color
-
 
 # Prints first argument as header. Additionally prints current date.
 shout() {
@@ -38,24 +36,13 @@ golangci::install() {
   shout "Install the golangci-lint in version ${GOLANGCI_LINT_VERSION}"
   curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | bash -s -- -b "${INSTALL_DIR}" ${GOLANGCI_LINT_VERSION}
   echo -e "${GREEN}√ install golangci-lint${NC}"
-
 }
 
 golangci::run_checks() {
   shout "Run golangci-lint checks"
-  LINTS=(
-    # default golangci-lint lints
-    deadcode errcheck gosimple govet ineffassign staticcheck \
-    structcheck typecheck unused varcheck \
-    # additional lints
-    revive gofmt misspell gochecknoinits unparam exportloopref gosec
-  )
 
-  ENABLE=$(sed 's/ /,/g' <<< "${LINTS[@]}")
-
-  echo "Checks: ${LINTS[*]}"
   cd ${ROOT_PATH}
-  golangci-lint run --disable-all --enable="${ENABLE}" --timeout=10m --config $CURRENT_DIR/.golangci.yml --skip-dirs-use-default
+  golangci-lint run -v --timeout=10m --config $CURRENT_DIR/.golangci.yml --skip-dirs-use-default
 
   echo -e "${GREEN}√ run golangci-lint${NC}"
 }
