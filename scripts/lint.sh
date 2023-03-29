85f0d9a0c8e33b5196c61774f8fa40ca84cdd721
Benjamin Lindner <benjamin.lindner@sap.com>
2023-03-29 14:08:43 +0200

    Test --concurrency 2

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -42,7 +42,7 @@ golangci::run_checks() {
   shout "Run golangci-lint checks"
 
   cd ${ROOT_PATH}
-  golangci-lint run -v --timeout=10m --config $CURRENT_DIR/.golangci.yml --skip-dirs-use-default --new-from-rev=$PULL_BASE_SHA --concurrency 4
+  golangci-lint run -v --timeout=10m --config $CURRENT_DIR/.golangci.yml --skip-dirs-use-default --new-from-rev=$PULL_BASE_SHA --concurrency 2
 
   echo -e "${GREEN}√ run golangci-lint${NC}"
 }
