7e8257318f716050a6066bc37f6e18e0e93fc472
Tobias Schuhmacher <t.schuhmacher@sap.com>
2023-06-23 10:13:39 +0200

    Ignore ISTIO CRDs if migrated CRD could be found on target cluster

--- pkg/reconciler/service/install.go
+++ pkg/reconciler/service/install.go
@@ -3,13 +3,17 @@ package service
 import (
 	"context"
 	"fmt"
-
 	"github.com/kyma-incubator/reconciler/pkg/model"
 	"github.com/kyma-incubator/reconciler/pkg/reconciler"
 	"github.com/kyma-incubator/reconciler/pkg/reconciler/chart"
 	"github.com/kyma-incubator/reconciler/pkg/reconciler/kubernetes"
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
+	k8serr "k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 type Install struct {
@@ -103,11 +107,57 @@ func (r *Install) renderManifest(chartProvider chart.Provider, model *reconciler
 }
 
 func (r *Install) renderCRDs(chartProvider chart.Provider, model *reconciler.Task) (string, error) {
-	crdManifests, err := chartProvider.RenderCRD(model.Version)
+	var crdManifests []*chart.Manifest
+	var err error
+	if r.ignoreIstioCRD(model) {
+		crdManifests, err = chartProvider.RenderCRDFiltered(model.Version, []string{"istio"})
+	} else {
+		crdManifests, err = chartProvider.RenderCRD(model.Version)
+	}
 	if err != nil {
 		msg := fmt.Sprintf("Failed to get CRD manifests for Kyma version '%s'", model.Version)
 		r.logger.Errorf("%s: %s", msg, err)
 		return "", errors.Wrap(err, msg)
 	}
 	return chart.MergeManifests(crdManifests...), nil
+}
+
+func (r *Install) ignoreIstioCRD(task *reconciler.Task) bool {
+	restConfig, err := clientcmd.NewClientConfigFromBytes([]byte(task.Kubeconfig))
+	if err != nil {
+		r.logger.Warnf("Failed to create K8s REST client to check for migrated Istio CRD: %s", err)
+		return true
+	}
+
+	clientConfig, err := restConfig.ClientConfig()
+	if err != nil {
+		r.logger.Warnf("Failed to create K8s client config to check for migrated Istio CRD: %s", err)
+		return true
+	}
+
+	kubeClient, err := dynamic.NewForConfig(clientConfig)
+	if err != nil {
+		r.logger.Warnf("Failed to create K8s kube client to check for migrated Istio CRD: %s", err)
+		return true
+	}
+
+	//check which of the configured CRDs exist on the cluster
+	group := "operator.kyma-project.io"
+	version := "v1alpha2"
+	kind := "istios"
+	gvr := schema.GroupVersionResource{
+		Group:    group,
+		Version:  version,
+		Resource: kind,
+	}
+	_, err = kubeClient.Resource(gvr).List(context.Background(), v1.ListOptions{})
+	if err == nil {
+		r.logger.Infof("Found obvioiusly migrated Istio CRD '%.%s:%s'", kind, group, version)
+		return true
+	} else if !k8serr.IsNotFound(err) {
+		r.logger.Errorf("Failed to retrieve CRD '%s.%s:%s': %s", kind, group, version, err)
+		return true
+	}
+
+	return false
 }
