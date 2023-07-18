3d4522e1218726a77501380b482f47ca82e9058f
Badr, Nesma <nesma.badr@sap.com>
2023-07-18 15:48:00 +0200

    remove token from cli-integration-latest.sh

--- scripts/prow/cli-integration-latest.sh
+++ scripts/prow/cli-integration-latest.sh
@@ -2,10 +2,7 @@ set -e
 apk add nodejs npm
 git remote add origin https://github.com/kyma-project/kyma.git
 git reset --hard && git remote update && git fetch --tags --all >/dev/null 2>&1
-test=$(git tag -l '[0-9]*.[0-9]*.[0-9]*'| sort -r -V | grep '^[^-rc]*$'| head -n1)
-echo "TEST:"$test
-kyma_get_last_release_version_return_version=$(curl --silent --fail --show-error -H "Authorization: token ${BOT_GITHUB_TOKEN}" "https://api.github.com/repos/kyma-project/kyma/releases" | jq -r 'del( .[] | select( (.prerelease == true) or (.draft == true) )) | sort_by(.tag_name | split(".") | map(tonumber)) | .[-1].target_commitish | split("/") | .[-1]')
-echo "Actual:"$kyma_get_last_release_version_return_version
+kyma_get_last_release_version_return_version=$(git tag -l '[0-9]*.[0-9]*.[0-9]*'| sort -r -V | grep '^[^-rc]*$'| head -n1)
 export KYMA_SOURCE="${kyma_get_last_release_version_return_version:?}"
 install_dir="/usr/local/bin"
 mkdir -p "$install_dir"
@@ -15,6 +12,5 @@ kyma version --client
 popd || exit
 kyma provision k3d --ci
 kyma deploy --ci --concurrency=8 --profile=evaluation --source="${KYMA_SOURCE}"
-git remote add origin https://github.com/kyma-project/kyma.git
 git reset --hard && git remote update && git fetch --all >/dev/null 2>&1 && git checkout "${KYMA_SOURCE}"
 make -C "../../kyma-project/kyma/tests/fast-integration" "ci"
