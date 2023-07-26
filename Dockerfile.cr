9be26c242ceab66b8f8efd2c1c10157d95f6b527
Bartosz Chwila <103247439+barchw@users.noreply.github.com>
2023-07-26 13:31:49 +0200

    Update Dockerfile.cr

--- Dockerfile.cr
+++ Dockerfile.cr
@@ -1,6 +1,6 @@
 # Istioctl source images
 FROM europe-docker.pkg.dev/kyma-project/prod/external/istio/istioctl:1.17.3 AS istio-1_17_3
-FROM europe-docker.pkg.dev/kyma-project/prod/external/istio/istioctl:1.18.1 AS istio-1_18_1
+FROM europe-docker.pkg.dev/kyma-project/prod/external/istio/istioctl:1.18.2 AS istio-1_18_2
 
 # Build image
 FROM golang:1.20.5-alpine3.17 AS build
@@ -40,9 +40,9 @@ COPY --from=build /configs/ /configs/
 
 # Add istioctl tools
 COPY --from=istio-1_17_3 /usr/local/bin/istioctl /bin/istioctl-1.17.3
-COPY --from=istio-1_18_1 /usr/local/bin/istioctl /bin/istioctl-1.18.1
+COPY --from=istio-1_18_2 /usr/local/bin/istioctl /bin/istioctl-1.18.2
 # For multiple istioctl binaries, provide their paths separated with a semicolon (;) like in the Linux PATH variable.
-ENV ISTIOCTL_PATH=/bin/istioctl-1.17.3;/bin/istioctl-1.18.1
+ENV ISTIOCTL_PATH=/bin/istioctl-1.17.3;/bin/istioctl-1.18.2
 
 USER appuser:appuser
 
