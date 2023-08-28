94a0c8a319833fac20a4151fc03f5ef6e139a8b9
Tobias Schuhmacher <t.schuhmacher@sap.com>
2023-08-28 17:24:41 +0200

    ignore CRDs of skipped components (#1388)
    * Ignore CRDs of skipped components
    
    * add test for env-var skip-comp logic
    
    * add logging
    
    * add correlation ID

---
+++ pkg/reconciler/service/install_test.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/kyma-incubator/reconciler/pkg/logger"
+	"github.com/kyma-incubator/reconciler/pkg/model"
+	"github.com/kyma-incubator/reconciler/pkg/reconciler"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInstall(t *testing.T) {
+	tests := []struct {
+		envVars  map[string]string
+		expected []string
+	}{
+		{
+			envVars: map[string]string{
+				"IM_INVALID": "true",
+				fmt.Sprintf("%s%s", model.SkippedComponentEnvVarPrefix, "ABC"):  "tRue",
+				fmt.Sprintf("%s%s", model.SkippedComponentEnvVarPrefix, "DE_F"): "1",
+				fmt.Sprintf("%s%s", model.SkippedComponentEnvVarPrefix, "GH"):   "0",
+				fmt.Sprintf("%s%s", model.SkippedComponentEnvVarPrefix, "XYZ"):  "truee",
+			},
+			expected: []string{"abc", "de-f"},
+		},
+	}
+
+	for _, testCase := range tests {
+		install := NewInstall(logger.NewTestLogger(t))
+		for envKey, envValue := range testCase.envVars {
+			os.Setenv(envKey, envValue)
+		}
+		got := install.skippedComps(&reconciler.Task{
+			CorrelationID: "unit-test-corrID",
+		})
+		require.Len(t, got, len(testCase.expected))
+		require.ElementsMatch(t, testCase.expected, got)
+	}
+}
