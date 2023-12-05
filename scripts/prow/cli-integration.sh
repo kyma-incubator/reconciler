66a87a4330962f6213789625313fdbde4526d645
Tobias Schuhmacher <t.schuhmacher@sap.com>
2023-12-05 15:20:51 +0100

    Remove fast integration tests (#1414)
    * remove fast integration tests
    
    * Remove merge comments
    
    * Remove fast-integration

--- scripts/prow/cli-integration.sh
+++ scripts/prow/cli-integration.sh
@@ -8,3 +8,4 @@ kyma version --client
 popd || exit
 kyma provision k3d --ci
 kyma deploy --ci --concurrency=8 --profile=evaluation --source=main
+#make -C "../../kyma-project/kyma/tests/fast-integration" "ci"
