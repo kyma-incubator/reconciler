d07839c904f34a83d2f126e55fbacd18a1318815
Tobias Schuhmacher <t.schuhmacher@sap.com>
2024-01-24 11:55:13 +0100

    add jitter

---
+++ pkg/cache/kubecConfig_test.go
@@ -0,0 +1,19 @@
+package cache
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetTTLWithinRange(t *testing.T) {
+	maxTTL := getTTL()
+	minMinutes := int64(maxTTL.Minutes() / 4)
+	maxMinutes := int64(maxTTL.Minutes())
+	for i := 0; i < 100; i++ {
+		randDurationMin := int64(getTTLWithinRange().Minutes())
+		require.True(t, randDurationMin <= maxMinutes && randDurationMin >= minMinutes,
+			"Value of %dmin is out of min-max range: %d - %d", randDurationMin, minMinutes, maxMinutes)
+		t.Log(randDurationMin)
+	}
+}
