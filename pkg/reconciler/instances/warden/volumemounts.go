e01826087e78a33b24dde3f66d863e8652f8ad5f
Kwiatosz, Krzysztof <krzysztof.kwiatosz@sap.com>
2024-01-26 15:44:15 +0100

    Warden volume mount cleanup reconciler

---
+++ pkg/reconciler/instances/warden/volumemounts.go
@@ -0,0 +1,95 @@
+package warden
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Masterminds/semver/v3"
+	"github.com/kyma-incubator/reconciler/pkg/reconciler/kubernetes"
+	"github.com/kyma-incubator/reconciler/pkg/reconciler/service"
+	"github.com/pkg/errors"
+	appsv1 "k8s.io/api/apps/v1"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+const wardenAdmissionDeploymentName = "warden-admission"
+const wardenAdmissionDeploymentNamespace = "kyma-system"
+const volumeName = "certs"
+
+// TODO: please implement component specific action logic here
+type CleanupWardenAdmissionCertColumeMounts struct {
+	name string
+}
+
+func (a *CleanupWardenAdmissionCertColumeMounts) Run(context *service.ActionContext) error {
+	k8sClient := context.KubeClient
+
+	context.Logger.Infof("Action '%s' executed (passed version was '%s')", a.name, context.Task.Version)
+
+	deployment, err := getDeployment(context.Context, k8sClient, wardenAdmissionDeploymentName, wardenAdmissionDeploymentNamespace)
+	if err != nil {
+		return errors.Wrap(err, fmt.Sprintf("while checking if %s deployment is present on cluster", wardenAdmissionDeploymentName))
+	}
+	if deployment != nil && isQualifiedForCleanup(*deployment) {
+
+		volumeIndex := getVolumeIndexByName(deployment, volumeName)
+		volumeMountIndex := getVolumeMountIndexByName(deployment, volumeName)
+
+		if volumeIndex == -1 || volumeMountIndex == -1 {
+			return nil
+		}
+
+		data := fmt.Sprintf(`[{"op": "remove", "path": "/spec/template/spec/containers/0/volumeMounts/%d"},{"op": "remove", "path": "/spec/template/spec/volumes/%d"}]`, volumeMountIndex, volumeIndex)
+		err = k8sClient.PatchUsingStrategy(context.Context, "Deployment", wardenAdmissionDeploymentName, wardenAdmissionDeploymentNamespace, []byte(data), types.StrategicMergePatchType)
+		if err != nil {
+			return errors.Wrap(err, fmt.Sprintf("while patching  %s deployment", wardenAdmissionDeploymentName))
+		}
+	}
+
+	return nil
+}
+
+func isQualifiedForCleanup(deployment appsv1.Deployment) bool {
+	wardenAdmissionImage := deployment.Spec.Template.Spec.Containers[0].Image
+	return isVersionQualifiedForCleanup(wardenAdmissionImage)
+}
+
+// Only 0.10.0 or higher versions qualify for cleanup
+func isVersionQualifiedForCleanup(versionToCheck string) bool {
+	version, err := semver.NewVersion(versionToCheck)
+	if err != nil {
+		return false //Non semver versions do not qualify for cleanup
+	}
+	targetVersion, _ := semver.NewVersion("0.10.0")
+	return version.Compare(targetVersion) >= 0
+}
+
+func getDeployment(context context.Context, kubeClient kubernetes.Client, namespace, name string) (*appsv1.Deployment, error) {
+	deployment, err := kubeClient.GetDeployment(context, name, namespace)
+	if err != nil {
+		if k8sErrors.IsNotFound(err) {
+			return nil, nil
+		}
+		return nil, errors.Wrap(err, fmt.Sprintf("while getting %s deployment", name))
+	}
+	return deployment, nil
+}
+
+func getVolumeIndexByName(deployment *appsv1.Deployment, volumeName string) int {
+	for p, v := range deployment.Spec.Template.Spec.Volumes {
+		if v.Name == volumeName {
+			return p
+		}
+	}
+	return -1
+}
+
+func getVolumeMountIndexByName(deployment *appsv1.Deployment, volumeMountName string) int {
+	for p, v := range deployment.Spec.Template.Spec.Containers[0].VolumeMounts {
+		if v.Name == volumeMountName {
+			return p
+		}
+	}
+	return -1
+}
